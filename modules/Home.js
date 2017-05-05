7dc5240988fc6d6f2bd8e5e92e6cc765810edcec
wchen <sdjcw13@gmail.com>
2017-05-05 11:01:39 +0800

    feat: 默认使用 tickets 作为首页

--- modules/Home.js
+++ modules/Home.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default React.createClass({
+  componentDidMount() {
+    this.context.router.push('/tickets')
+  },
+  contextTypes: {
+    router: React.PropTypes.object
+  },
   render() {
     return <div>Home</div>
   }
