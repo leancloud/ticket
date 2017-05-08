fbcf266ffad0dbea2d7bac17bde8c36e812431e6
wchen <sdjcw13@gmail.com>
2017-05-08 17:35:22 +0800

    fix: 修复移动设备上导航条按钮消失的问题

--- modules/GlobalNav.js
+++ modules/GlobalNav.js
@@ -43,9 +43,15 @@ export default React.createClass({
       <nav className="navbar navbar-default">
         <div className="container-fluid">
           <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#global-navbar-collapse" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
             <a className="navbar-brand" href="/">LeanTicket</a>
           </div>
-          <div className="collapse navbar-collapse">
+          <div className="collapse navbar-collapse" id="global-navbar-collapse">
             <ul className="nav navbar-nav">
               <li><Link to="/tickets">工单列表</Link></li>
               <li><Link to="/about">关于</Link></li>
