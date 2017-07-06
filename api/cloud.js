9abea82863e06ac03b6a6b736f2a3a03a1144b2d
wchen <sdjcw13@gmail.com>
2017-07-06 15:36:03 +0800

    fix: 增加一个简单的数据订正方法

--- api/cloud.js
+++ api/cloud.js
@@ -1,8 +1,34 @@
 const AV = require('leanengine')
 
+const common = require('./common')
+
 /**
  * 一个简单的云代码方法
  */
 AV.Cloud.define('hello', function(request, response) {
   response.success('Hello world!')
 })
+
+AV.Cloud.define('contentHtml', (_req) => {
+  return addHtml()
+})
+
+const addHtml = () => {
+  return new AV.Query('Reply')
+  .doesNotExist('content_HTML')
+  .select('content')
+  .limit(1000)
+  .find({useMasterKey: true})
+  .then(objs => {
+    if (objs.length === 0) {
+      return 'done'
+    }
+    objs.forEach(obj => {
+      obj.set('content_HTML', common.htmlify(obj.get('content')))
+    })
+    return AV.Object.saveAll(objs, {useMasterKey: true})
+    .then(() => {
+      return addHtml()
+    })
+  })
+}
