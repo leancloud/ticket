0bcd1272e7a3057e0f5a23bc5c746ff4be967c7a
Tunghsiao Liu <t@sparanoid.com>
2017-07-11 16:19:58 +0800

    feat(error): simplify error page layout

--- modules/Error.js
+++ modules/Error.js
@@ -6,8 +6,9 @@ export default function Error(props) {
 
   if (!props.location.state) {
     return (
-      <div className="jumbotron">
-        <h1>这里是错误页面</h1>
+      <div>
+        <h1 className='font-logo'>这里是错误页面</h1>
+        <hr />
         <p>不过好像没有抓到什么错误信息</p>
         <p>如有疑问，可以通过 <a href="mailto:support@leancloud.cn">support@leancloud.cn</a> 联系我们</p>
       </div>
@@ -15,7 +16,7 @@ export default function Error(props) {
   }
 
   switch (props.location.state.code) {
-  case 'requireCustomerServiceAuth': 
+  case 'requireCustomerServiceAuth':
     message = '您访问的页面需要技术支持人员权限。'
     break
   case 'Unauthorized':
@@ -25,8 +26,9 @@ export default function Error(props) {
     message = '未知信息: ' + this.props.location.state.code
   }
   return (
-    <div className="jumbotron">
-      <h1>很抱歉，看起来出了一些问题</h1>
+    <div>
+      <h1 className='font-logo'>很抱歉，看起来出了一些问题</h1>
+      <hr />
       <p>{message}</p>
       <p>如有疑问，可以通过 <a href="mailto:support@leancloud.cn">support@leancloud.cn</a> 联系我们</p>
     </div>
