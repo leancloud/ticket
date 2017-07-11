78e8f50251f703d05ec5c1e2e4253f6e131139a9
Tunghsiao Liu <t@sparanoid.com>
2017-07-11 17:51:12 +0800

    feat(index): try html anchors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { anchorate } from 'anchorate'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 
 import routes from './modules/routes'
 
+function onUpdate () {
+  anchorate()
+}
+
 render((
-  <Router routes={routes} history={browserHistory}/>
+  <Router routes={routes} history={browserHistory} onUpdate={onUpdate} />
 ), document.getElementById('app'))
