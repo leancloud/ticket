leancloud/ticket

commit 55f575b2ab77bc695f5c3a49a1a00219d914c19c

feat: 客服工单列表页增加工单状态过滤

modules/CustomerServiceTickets.js
@@ -7,7 +7,8 @@ import moment from 'moment'
 import AV from 'leancloud-storage/live-query'
 import css from './CustomerServiceTickets.css'
 
-import {sortTickets, UserLabel, TicketStatusLabel, getCustomerServices, ticketOpenedStatuses, ticketClosedStatuses} from './common'
+import {UserLabel, TicketStatusLabel, getCustomerServices, ticketOpenedStatuses, ticketClosedStatuses} from './common'
+import {TICKET_STATUS, TICKET_STATUS_MSG} from '../lib/constant'
 
 let authorSearchTimeoutId
 
@@ -57,14 +58,25 @@ export default class CustomerServiceTickets extends Component {
     })
   }
 
-  findTickets({assigneeId, isOpen, categoryId, authorId, isOnlyUnlike, page = '0', size = '10'}) {
-    let query = new AV.Query('Ticket')
+  findTickets({assigneeId, isOpen, status, categoryId, authorId, isOnlyUnlike, page = '0', size = '10'}) {
+    let statuses = []
+    if (isOpen === 'true') {
+      statuses = ticketOpenedStatuses()
+    } else if (isOpen === 'false') {
+      statuses = ticketClosedStatuses()
+    } else if (status) {
+      statuses = [parseInt(status)]
+    }
 
-    const queryFilters = (isOpen === 'true' ? ticketOpenedStatuses() : ticketClosedStatuses())
-    .map((status) => {
-      return new AV.Query('Ticket').equalTo('status', status)
-    })
-    query = AV.Query.or(...queryFilters)
+    let query
+    if (statuses.length === 0) {
+      query = new AV.Query('Ticket')
+    } else {
+      const statusFilters = statuses.map((status) => {
+        return new AV.Query('Ticket').equalTo('status', status)
+      })
+      query = AV.Query.or(...statusFilters)
+    }
 
     if (assigneeId) {
       query.equalTo('assignee', AV.Object.createWithoutData('_User', assigneeId))
@@ -142,7 +154,7 @@ export default class CustomerServiceTickets extends Component {
 
   render() {
     const filters = this.props.location.query
-    const tickets = sortTickets(this.state.tickets)
+    const tickets = this.state.tickets
     const ticketTrs = tickets.map((ticket) => {
       const customerServices = (ticket.get('joinedCustomerServices') || []).map((user) => {
         return (
@@ -187,12 +199,28 @@ export default class CustomerServiceTickets extends Component {
         </div>
       )
     })
+
+    const statusMenuItems = Object.keys(TICKET_STATUS).map(key => {
+      const value = TICKET_STATUS[key]
+      return <MenuItem key={value} eventKey={value}>{TICKET_STATUS_MSG[value]}</MenuItem>
+    })
     const assigneeMenuItems = this.state.customerServices.map((user) => {
       return <MenuItem key={user.id} eventKey={user.id}>{user.get('username')}</MenuItem>
     })
     const categoryMenuItems = this.state.categories.map((category) => {
       return <MenuItem key={category.id} eventKey={category.id}>{category.get('name')}</MenuItem>
     })
+
+    let statusTitle
+    if (filters.status) {
+      statusTitle = TICKET_STATUS_MSG[filters.status]
+    } else if (filters.isOpen === 'true') {
+      statusTitle = '未完成'
+    } else if (filters.isOpen === 'false') {
+      statusTitle = '已完成'
+    } else {
+      statusTitle = '全部状态'
+    }
 
     let assigneeTitle
     if (filters.assigneeId) {
@@ -223,8 +251,12 @@ export default class CustomerServiceTickets extends Component {
         <FormGroup>
           <ButtonToolbar>
             <ButtonGroup>
-              <button className={'btn btn-default' + (filters.isOpen === 'true' ? ' active' : '')} onClick={() => this.updateFilter({isOpen: true})}>未完成</button>
-              <button className={'btn btn-default' + (filters.isOpen === 'true' ? '' : ' active')} onClick={() => this.updateFilter({isOpen: false})}>已完成</button>
+              <button className={'btn btn-default' + (filters.isOpen === 'true' ? ' active' : '')} onClick={() => this.updateFilter({isOpen: true, status: undefined})}>未完成</button>
+              <button className={'btn btn-default' + (filters.isOpen === 'false' ? ' active' : '')} onClick={() => this.updateFilter({isOpen: false, status: undefined})}>已完成</button>
+              <DropdownButton className={(filters.status ? ' active' : '')} id='statusDropdown' title={statusTitle} onSelect={(eventKey) => this.updateFilter({status: eventKey, isOpen: undefined})}>
+                <MenuItem key='undefined'>全部状态</MenuItem>
+                {statusMenuItems}
+              </DropdownButton>
             </ButtonGroup>
             <ButtonGroup>
               <Button className={(filters.assigneeId === AV.User.current().id ? ' active' : '')} onClick={() => this.updateFilter({assigneeId: AV.User.current().id})}>分配给我的</Button>

lib/constant.js
@@ -1,13 +1,20 @@
-module.exports = {
-  TICKET_STATUS: {
-    // 0~99 未开始处理
-    NEW: 50, // 新工单，还没有技术支持人员回复
-    // 100~199 处理中
-    WAITING_CUSTOMER_SERVICE: 120,
-    WAITING_CUSTOMER: 160,
-    // 200~299 处理完成
-    PRE_FULFILLED: 220, // 技术支持人员点击“解决”时会设置该状态，用户确认后状态变更为 FULFILLED
-    FULFILLED: 250, // 已解决
-    REJECTED: 280, // 已关闭
-  }
+exports.TICKET_STATUS = {
+  // 0~99 未开始处理
+  NEW: 50, // 新工单，还没有技术支持人员回复
+  // 100~199 处理中
+  WAITING_CUSTOMER_SERVICE: 120,
+  WAITING_CUSTOMER: 160,
+  // 200~299 处理完成
+  PRE_FULFILLED: 220, // 技术支持人员点击“解决”时会设置该状态，用户确认后状态变更为 FULFILLED
+  FULFILLED: 250, // 已解决
+  REJECTED: 280, // 已关闭
+}
+
+exports.TICKET_STATUS_MSG = {
+  [exports.TICKET_STATUS.NEW]: '待处理',
+  [exports.TICKET_STATUS.WAITING_CUSTOMER_SERVICE]: '等待客服回复',
+  [exports.TICKET_STATUS.WAITING_CUSTOMER]: '等待用户回复',
+  [exports.TICKET_STATUS.PRE_FULFILLED]: '待确认解决',
+  [exports.TICKET_STATUS.FULFILLED]: '已解决',
+  [exports.TICKET_STATUS.REJECTED]: '已关闭',
 }

modules/Tickets.js
@@ -6,7 +6,7 @@ import moment from 'moment'
 import AV from 'leancloud-storage/live-query'
 import css from './CustomerServiceTickets.css'
 
-import {sortTicketsForCustomer, UserLabel, TicketStatusLabel} from './common'
+import {UserLabel, TicketStatusLabel} from './common'
 
 export default class Tickets extends Component {
 
@@ -42,8 +42,7 @@ export default class Tickets extends Component {
   }
 
   render() {
-    const tickets = sortTicketsForCustomer(this.state.tickets)
-    const ticketLinks = tickets.map((ticket) => {
+    const ticketLinks = this.state.tickets.map((ticket) => {
       let latestReply = ticket.get('latestReply')
       let latestReplyContent = ''
       if (latestReply && latestReply.content) {

modules/common.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router'
 import _ from 'lodash'
 import AV from 'leancloud-storage/live-query'
 
-const TICKET_STATUS = require('../lib/constant').TICKET_STATUS
+const {TICKET_STATUS, TICKET_STATUS_MSG} = require('../lib/constant')
 
 exports.getTinyCategoryInfo = (category) => {
   return {
@@ -114,46 +114,6 @@ exports.getTicketAndRelation = (nid) => {
   })
 }
 
-exports.sortTickets = (tickets) => {
-  return _.sortBy(tickets, (ticket) => {
-    switch (ticket.get('status')) {
-    case TICKET_STATUS.NEW:
-      return 0
-    case TICKET_STATUS.WAITING_CUSTOMER_SERVICE:
-      return 1
-    case TICKET_STATUS.WAITING_CUSTOMER:
-      return 2
-    case TICKET_STATUS.PRE_FULFILLED:
-      return 3
-    case TICKET_STATUS.FULFILLED:
-    case TICKET_STATUS.REJECTED:
-      return 4
-    default:
-      new Error('unkonwn ticket status:', ticket.get('status'))
-    }
-  })
-}
-
-exports.sortTicketsForCustomer = (tickets) => {
-  return _.sortBy(tickets, (ticket) => {
-    switch (ticket.get('status')) {
-    case TICKET_STATUS.WAITING_CUSTOMER:
-      return 0
-    case TICKET_STATUS.PRE_FULFILLED:
-      return 1
-    case TICKET_STATUS.NEW:
-      return 2
-    case TICKET_STATUS.WAITING_CUSTOMER_SERVICE:
-      return 3
-    case TICKET_STATUS.FULFILLED:
-    case TICKET_STATUS.REJECTED:
-      return 4
-    default:
-      new Error('unkonwn ticket status:', ticket.get('status'))
-    }
-  })
-}
-
 exports.UserLabel = (props) => {
   if (!props.user) {
     return (
@@ -183,18 +143,21 @@ exports.UserLabel.propTypes = {
 }
 
 exports.TicketStatusLabel = (props) => {
-  if (props.status === TICKET_STATUS.FULFILLED) {
-    return <span className='label label-success'>已解决</span>
-  } else if (props.status === TICKET_STATUS.REJECTED) {
-    return <span className='label label-default'>已关闭</span>
-  } else if (props.status === TICKET_STATUS.PRE_FULFILLED) {
-    return <span className='label label-primary'>待确认解决</span>
-  } else if (props.status === TICKET_STATUS.NEW) {
-    return <span className='label label-danger'>待处理</span>
-  } else if (props.status === TICKET_STATUS.WAITING_CUSTOMER_SERVICE) {
-    return <span className='label label-warning'>等待客服回复</span>
-  } else if (props.status === TICKET_STATUS.WAITING_CUSTOMER) {
-    return <span className='label label-primary'>等待用户回复</span>
+  switch (props.status) {
+  case TICKET_STATUS.FULFILLED:
+    return <span className='label label-success'>{TICKET_STATUS_MSG[props.status]}</span>
+  case TICKET_STATUS.REJECTED:
+    return <span className='label label-default'>{TICKET_STATUS_MSG[props.status]}</span>
+  case TICKET_STATUS.PRE_FULFILLED:
+    return <span className='label label-primary'>{TICKET_STATUS_MSG[props.status]}</span>
+  case TICKET_STATUS.NEW:
+    return <span className='label label-danger'>{TICKET_STATUS_MSG[props.status]}</span>
+  case TICKET_STATUS.WAITING_CUSTOMER_SERVICE:
+    return <span className='label label-warning'>{TICKET_STATUS_MSG[props.status]}</span>
+  case TICKET_STATUS.WAITING_CUSTOMER:
+    return <span className='label label-primary'>{TICKET_STATUS_MSG[props.status]}</span>
+  default:
+    new Error('unkonwn ticket status:', props.status)
   }
 }
 exports.TicketStatusLabel.displayName = 'TicketStatusLabel'

commit 05da81cf1cf61d9d053ebdad82dc62f52845c22f

fix(CustomerServiceTickets): wrong class state condition introduced in https://github.com/leancloud/ticket/commit/55f575b2ab77bc695f5c3a49a1a00219d914c19c

modules/CustomerServiceTickets.js
@@ -256,14 +256,14 @@ export default class CustomerServiceTickets extends Component {
             <ButtonGroup>
               <button className={'btn btn-default' + (filters.isOpen === 'true' ? ' active' : '')} onClick={() => this.updateFilter({isOpen: true, status: undefined})}>未完成</button>
               <button className={'btn btn-default' + (filters.isOpen === 'false' ? ' active' : '')} onClick={() => this.updateFilter({isOpen: false, status: undefined})}>已完成</button>
-              <DropdownButton className={(filters.status ? ' active' : '')} id='statusDropdown' title={statusTitle} onSelect={(eventKey) => this.updateFilter({status: eventKey, isOpen: undefined})}>
+              <DropdownButton className={(typeof filters.isOpen === 'undefined' ? ' active' : '')} id='statusDropdown' title={statusTitle} onSelect={(eventKey) => this.updateFilter({status: eventKey, isOpen: undefined})}>
                 <MenuItem key='undefined'>全部状态</MenuItem>
                 {statusMenuItems}
               </DropdownButton>
             </ButtonGroup>
             <ButtonGroup>
               <Button className={(filters.assigneeId === AV.User.current().id ? ' active' : '')} onClick={() => this.updateFilter({assigneeId: AV.User.current().id})}>分配给我的</Button>
-              <DropdownButton className={(filters.assigneeId && filters.assigneeId !== AV.User.current().id ? ' active' : '')} id='assigneeDropdown' title={assigneeTitle} onSelect={(eventKey) => this.updateFilter({assigneeId: eventKey})}>
+              <DropdownButton className={(typeof filters.assigneeId === 'undefined' || filters.assigneeId && filters.assigneeId !== AV.User.current().id ? ' active' : '')} id='assigneeDropdown' title={assigneeTitle} onSelect={(eventKey) => this.updateFilter({assigneeId: eventKey})}>
                 <MenuItem key='undefined'>全部客服</MenuItem>
                 {assigneeMenuItems}
               </DropdownButton>