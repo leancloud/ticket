f295eedf2c1852a80b25d2efcf69c42a0495ae04
Da Li <myleslee@gmail.com>
2017-10-20 02:43:15 -0500

    chore: fix typos and rephrase (#9)
    * chore: fix typos and rephrase
    
    * add a missing space

--- modules/About.js
+++ modules/About.js
@@ -6,8 +6,8 @@ export default function About() {
     <DocumentTitle title='关于 - LeanTicket' />
     <h1 className='font-logo'>LeanCloud Ticket</h1>
     <hr />
-    <p>该应用是 <a href='https://leancloud.cn/'>LeanCloud</a> 的工单系统。为了更有效的解决 LeanCloud 开发者的问题而创建。</p>
-    <p>同时，该应用也是一个示例应用（<a href='https://github.com/leancloud/ticket'>源代码</a>），可以帮助开发者了解如何使用 LeanCloud 的各项服务，以及一些特定场景的解决方案。</p>
+    <p>该应用是 <a href='https://leancloud.cn/'>LeanCloud</a> 的工单系统，为更有效地解决 LeanCloud 开发者的问题而创建。</p>
+    <p>该应用开放 <a href='https://github.com/leancloud/ticket'>源代码</a>，旨在帮助开发者了解 LeanCloud 各项服务的使用方法以及一些特定场景的解决方案。</p>
   </div>
 }
 
