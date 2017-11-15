31b49b38938b2c98c5fe42014b02d9f51ba9d29d
Cui Yiran <cuiyiran@cuiyirandeMacBook-Pro.local>
2017-11-15 16:10:27 +0800

    fix: Notify the customer services while the last opsLog is out of date

--- api/notify.js
+++ api/notify.js
@@ -61,9 +61,9 @@ const delayNotify = () => {
       .find({useMasterKey: true})
       .then((opsLogs) => {
         const opsLog = opsLogs[0];
+        const assignee = ticket.get('assignee');
         if (opsLog.get('action') !== 'replySoon') {
           // the ticket which is being progressed do not need notify
-          const assignee = ticket.get('assignee');
           return Promise.all([
             mail.delayNotify(ticket, assignee),
             bearychat.delayNotify(ticket, assignee),
