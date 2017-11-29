9d65282a2ee05751386ccd0751eafae160c01800
wchen <sdjcw13@gmail.com>
2017-11-30 02:01:19 +0800

    doc: about 页面增加更新记录

--- modules/About.js
+++ modules/About.js
@@ -8,6 +8,15 @@ export default function About() {
     <hr />
     <p>该应用是 <a href='https://leancloud.cn/'>LeanCloud</a> 的工单系统，为更有效地解决 LeanCloud 开发者的问题而创建。</p>
     <p>该应用开放 <a href='https://github.com/leancloud/ticket'>源代码</a>，旨在帮助开发者了解 LeanCloud 各项服务的使用方法以及一些特定场景的解决方案。</p>
+    <h2>更新记录</h2>
+    <h3>v1.1.0</h3>
+    <ul>
+      <li>特性：增加「个人设置」页面，可以修改昵称、邮箱等个人信息。</li>
+    </ul>
+    <h3>v1.0.0</h3>
+    <ul>
+      <li>基本功能完成。</li>
+    </ul>
   </div>
 }
 
