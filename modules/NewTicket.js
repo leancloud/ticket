8501562758d501edbfa12d7ed86a147302caab86
wchen <sdjcw13@gmail.com>
2017-12-06 11:04:21 +0800

    fix(client): 修复 ServerNotification 组件 currentUser 获取错误的问题

--- modules/NewTicket.js
+++ modules/NewTicket.js
@@ -98,17 +98,18 @@ export default class NewTicket extends React.Component {
         content: this.state.content,
         files,
       })
-    }).then(() => {
+    })
+    .then(() => {
+      this.setState({isCommitting: false})
+      return
+    })
+    .then(() => {
       localStorage.removeItem('ticket:new:title')
       localStorage.removeItem('ticket:new:content')
       this.context.router.push('/tickets')
       return
     })
     .catch(this.context.addNotification)
-    .then(() => {
-      this.setState({isCommitting: false})
-      return
-    })
   }
 
   render() {
