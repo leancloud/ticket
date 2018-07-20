90e914d017a4e3e75263b55a735b55535e41d05c
wchen <sdjcw13@gmail.com>
2018-07-20 11:15:02 -0400

    chore: 修复 migrate  脚本的问题

--- migrations/1511857499214-add-avatar.js
+++ migrations/1511857499214-add-avatar.js
@@ -1,24 +1,26 @@
 const AV = require('leanengine')
 
-const {getGravatarHash} = require('../../lib/common')
-const forEachAVObject = require('../../api/common').forEachAVObject
+const {getGravatarHash} = require('../lib/common')
+const forEachAVObject = require('../api/common').forEachAVObject
 
 exports.up = function(next) {
   return forEachAVObject(new AV.Query('_User'), (user) => {
     return user.set('gravatarHash', getGravatarHash(user.get('email')))
-    .save()
+    .save(null, {useMasterKey: true})
   })
   .then(() => {
     return next()
   })
+  .catch(next)
 }
 
 exports.down = function(next) {
   return forEachAVObject(new AV.Query('_User'), (user) => {
     return user.unset('gravatarHash')
-    .save()
+    .save(null, {useMasterKey: true})
   })
   .then(() => {
     return next()
   })
+  .catch(next)
 }
