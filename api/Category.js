88747ad446eeb3a2018db05c0b3bc887de825089
wchen <sdjcw13@gmail.com>
2018-08-02 00:16:49 -0400

    fix: 删除分类时增加更多限制
    * 分类不能有相关负责人
    * 分类不能有工单
    * 分类不能有子分类

--- api/Category.js
+++ api/Category.js
@@ -14,18 +14,28 @@ AV.Cloud.beforeSave('Category', (req, res) => {
   }).catch(errorHandler.captureException)
 })
 
-AV.Cloud.beforeDelete('Category', (req) => {
+AV.Cloud.beforeDelete('Category', async (req) => {
   const category = req.object
-  return new AV.Query('_User')
-  .equalTo('categories.objectId', category.id)
-  .limit(1000)
-  .find({user: req.currentUser})
-  .then((users) => {
-    if (users.length > 0) {
-      throw new AV.Cloud.Error('仍有人负责该分类，不能删除。')
-    }
-    return
-  })
+  const user = await new AV.Query('_User')
+    .equalTo('categories.objectId', category.id)
+    .first({user: req.currentUser})
+  if (user) {
+    throw new AV.Cloud.Error('仍有人负责该分类，不能删除。')
+  }
+
+  const ticket = await new AV.Query('Ticket')
+    .equalTo('category.objectId', category.id)
+    .first({user: req.currentUser})
+  if (ticket) {
+    throw new AV.Cloud.Error('仍有工单属于该分类，不能删除。')
+  }
+
+  const categoryChild = await new AV.Query('Category')
+    .equalTo('parent', AV.Object.createWithoutData('Category', category.id))
+    .first({user: req.currentUser})
+  if (categoryChild) {
+    throw new AV.Cloud.Error('仍有子分类属于该分类，不能删除。')
+  }
 })
 
 const getCategoryAcl = () => {
