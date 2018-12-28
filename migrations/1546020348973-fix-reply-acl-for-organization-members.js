2813b181ea7c8ec80f7dee19d75ecc6a3dacad4b
Siyuan Miao <i@xswan.net>
2018-12-29 01:59:35 +0800

    fix: 工单回复其他组织成员不可见

---
+++ migrations/1546020348973-fix-reply-acl-for-organization-members.js
@@ -0,0 +1,41 @@
+const AV = require('leanengine')
+
+const {forEachAVObject,getOrganizationRoleName} = require('../api/common')
+
+exports.up = function(next) {
+  const query = new AV.Query('Reply')
+  query.include('ticket')
+
+  return forEachAVObject(query, (reply) => {
+    const ticket = reply.get('ticket')
+    const organization = ticket.get('organization')
+
+    // don't update tickets without organization field
+    if (!organization) return
+
+    // fix acl for organization users
+    const acl = new AV.ACL()
+    const ticketAuthor = ticket.get('author')
+    const replyAuthor = reply.get('author')
+
+    acl.setWriteAccess(replyAuthor, true)
+    acl.setReadAccess(replyAuthor, true)
+    acl.setReadAccess(ticketAuthor, true)
+
+    const organizationRoleName = getOrganizationRoleName(organization)
+    acl.setRoleReadAccess(organizationRoleName, true)
+
+    acl.setRoleReadAccess(new AV.Role('customerService'), true)
+
+    reply.setACL(acl)
+    return reply.save(null, {useMasterKey: true})
+  })
+  .then(() => {
+    return next()
+  })
+  .catch(next)
+}
+
+exports.down = function(next) {
+  next()
+}
