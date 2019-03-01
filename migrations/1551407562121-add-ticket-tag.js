0da025ca726f77cc972c69bdda900f38aacea5be
wchen <sdjcw13@gmail.com>
2019-02-28 21:52:54 -0500

    chore: 增加标签相关的 schema 和数据库升级脚本

---
+++ migrations/1551407562121-add-ticket-tag.js
@@ -0,0 +1,37 @@
+'use strict'
+const AV = require('leanengine')
+
+module.exports.up = function (next) {
+  return new AV.Object('TagMetadata', {
+    key: 'testTag',
+    values: ['value1', 'value2'],
+    type: 'select',
+    isPrivate: true,
+  })
+  .save(null, {useMasterKey: true})
+  .then((m) => {
+    return m.destroy()
+  })
+  .then(() => {
+    const ticket = new AV.Object('Ticket', {
+      privateTags: [],
+      tags: [],
+    })
+    ticket.disableBeforeHook()
+    ticket.disableAfterHook()
+    return ticket.save(null, {useMasterKey: true})
+    .then(() => {
+      return ticket.destroy()
+    })
+    .then(() => {
+      return next()
+    })
+    .catch(err => {
+      return next(err)
+    })
+  })
+}
+
+module.exports.down = function (next) {
+  next()
+}
