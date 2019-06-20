8b48e4bc0cfb69cf826d4fbc23bf627d609416f8
Lee Yeh <lee55962698@gmail.com>
2019-06-20 19:42:12 +0800

    fix: 替换客户端访问域名为临时域名

--- modules/routes.js
+++ modules/routes.js
@@ -50,6 +50,7 @@ if (SENTRY_DSN_PUBLIC !== '') {
 AV.init({
   appId: LEANCLOUD_APP_ID,
   appKey: LEANCLOUD_APP_KEY,
+  serverURLs: 'https://avoscloud.com',
 })
 if (LEANCLOUD_APP_ENV === 'development') {
   AV.setProduction(LEAN_CLI_HAVE_STAGING !== 'true')
