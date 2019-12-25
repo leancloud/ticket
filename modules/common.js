42bda8c1cb355f5398d864ae6c26329a751ecb82
Hong Jiang <j@1byte.io>
2019-12-25 17:57:12 +0800

    fix: do not show CN company and ICP info on US site

--- modules/common.js
+++ modules/common.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router'
-import {Image, FormGroup, FormControl, Form, ControlLabel, Button} from 'react-bootstrap'
+import { Link } from 'react-router'
+import {
+  Image,
+  FormGroup,
+  FormControl,
+  Form,
+  ControlLabel,
+  Button
+} from 'react-bootstrap'
 import _ from 'lodash'
 import validUrl from 'valid-url'
 import AV from 'leancloud-storage/live-query'
@@ -9,8 +16,14 @@ import AV from 'leancloud-storage/live-query'
 Object.assign(exports, require('../lib/common'))
 
 exports.getCategoryPathName = (category, categoriesTree) => {
-  const c = exports.depthFirstSearchFind(categoriesTree, c => c.id == (category.id || category.objectId))
-  return exports.getNodePath(c).map(c => exports.getCategoryName(c)).join(' / ')
+  const c = exports.depthFirstSearchFind(
+    categoriesTree,
+    c => c.id == (category.id || category.objectId)
+  )
+  return exports
+    .getNodePath(c)
+    .map(c => exports.getCategoryName(c))
+    .join(' / ')
 }
 
 exports.requireAuth = (nextState, replace) => {
@@ -23,38 +36,40 @@ exports.requireAuth = (nextState, replace) => {
 }
 
 exports.requireCustomerServiceAuth = (nextState, replace, next) => {
-  exports.isCustomerService(AV.User.current())
-  .then((isCustomerService) => {
-    if (!isCustomerService) {
+  exports
+    .isCustomerService(AV.User.current())
+    .then(isCustomerService => {
+      if (!isCustomerService) {
+        replace({
+          pathname: '/error',
+          state: { code: 'requireCustomerServiceAuth' }
+        })
+      }
+      return next()
+    })
+    .catch(err => {
       replace({
         pathname: '/error',
-        state: { code: 'requireCustomerServiceAuth' }
+        state: { code: err.code, err }
       })
-    }
-    return next()
-  })
-  .catch((err) => {
-    replace({
-      pathname: '/error',
-      state: { code: err.code, err }
+      next()
     })
-    next()
-  })
 }
 
 exports.getCustomerServices = () => {
   return new AV.Query(AV.Role)
     .equalTo('name', 'customerService')
     .first()
-    .then((role) => {
-      return role.getUsers()
+    .then(role => {
+      return role
+        .getUsers()
         .query()
         .ascending('username')
         .find()
     })
 }
 
-exports.isCustomerService = (user) => {
+exports.isCustomerService = user => {
   if (!user) {
     return Promise.resolve(false)
   }
@@ -62,49 +77,51 @@ exports.isCustomerService = (user) => {
     .equalTo('name', 'customerService')
     .equalTo('users', user)
     .first()
-    .then((role) => {
+    .then(role => {
       return !!role
     })
 }
 
-exports.uploadFiles = (files) => {
-  return Promise.all(_.map(files, file => new AV.File(file.name, file).save({
-    keepFileName: true,
-  })))
-}
-
-exports.getTicketAndRelation = (nid) => {
-  return new AV.Query('Ticket')
-  .equalTo('nid', parseInt(nid))
-  .include('author')
-  .include('files')
-  .first()
-  .then((ticket) => {
-    if (!ticket) {
-      return
-    }
-    return Promise.all([
-      new AV.Query('Reply')
-        .equalTo('ticket', ticket)
-        .include('author')
-        .include('files')
-        .ascending('createdAt')
-        .find(),
-      new AV.Query('OpsLog')
-        .equalTo('ticket', ticket)
-        .ascending('createdAt')
-        .find(),
-    ]).spread((replies, opsLogs) => {
-      return {ticket, replies, opsLogs}
-    })
-  })
-}
-
-exports.UserLabel = (props) => {
-  if (!props.user) {
-    return (
-      <span>data err</span>
+exports.uploadFiles = files => {
+  return Promise.all(
+    _.map(files, file =>
+      new AV.File(file.name, file).save({
+        keepFileName: true
+      })
     )
+  )
+}
+
+exports.getTicketAndRelation = nid => {
+  return new AV.Query('Ticket')
+    .equalTo('nid', parseInt(nid))
+    .include('author')
+    .include('files')
+    .first()
+    .then(ticket => {
+      if (!ticket) {
+        return
+      }
+      return Promise.all([
+        new AV.Query('Reply')
+          .equalTo('ticket', ticket)
+          .include('author')
+          .include('files')
+          .ascending('createdAt')
+          .find(),
+        new AV.Query('OpsLog')
+          .equalTo('ticket', ticket)
+          .ascending('createdAt')
+          .find()
+      ]).spread((replies, opsLogs) => {
+        return { ticket, replies, opsLogs }
+      })
+    })
+}
+
+exports.UserLabel = props => {
+  if (!props.user) {
+    return <span>data err</span>
   }
   const username = props.user.username || props.user.get('username')
   const name = props.user.name || exports.getUserDisplayName(props.user)
@@ -128,35 +145,67 @@ exports.UserLabel = (props) => {
 exports.UserLabel.displayName = 'UserLabel'
 exports.UserLabel.propTypes = {
   user: PropTypes.object,
-  simple: PropTypes.bool,
+  simple: PropTypes.bool
 }
 
-exports.TicketStatusLabel = (props) => {
+exports.TicketStatusLabel = props => {
   switch (props.status) {
-  case exports.TICKET_STATUS.FULFILLED:
-    return <span className='label label-success'>{exports.TICKET_STATUS_MSG[props.status]}</span>
-  case exports.TICKET_STATUS.REJECTED:
-    return <span className='label label-default'>{exports.TICKET_STATUS_MSG[props.status]}</span>
-  case exports.TICKET_STATUS.PRE_FULFILLED:
-    return <span className='label label-primary'>{exports.TICKET_STATUS_MSG[props.status]}</span>
-  case exports.TICKET_STATUS.NEW:
-    return <span className='label label-danger'>{exports.TICKET_STATUS_MSG[props.status]}</span>
-  case exports.TICKET_STATUS.WAITING_CUSTOMER_SERVICE:
-    return <span className='label label-warning'>{exports.TICKET_STATUS_MSG[props.status]}</span>
-  case exports.TICKET_STATUS.WAITING_CUSTOMER:
-    return <span className='label label-primary'>{exports.TICKET_STATUS_MSG[props.status]}</span>
-  default:
-    throw new Error('unkonwn ticket status:', props.status)
+    case exports.TICKET_STATUS.FULFILLED:
+      return (
+        <span className="label label-success">
+          {exports.TICKET_STATUS_MSG[props.status]}
+        </span>
+      )
+    case exports.TICKET_STATUS.REJECTED:
+      return (
+        <span className="label label-default">
+          {exports.TICKET_STATUS_MSG[props.status]}
+        </span>
+      )
+    case exports.TICKET_STATUS.PRE_FULFILLED:
+      return (
+        <span className="label label-primary">
+          {exports.TICKET_STATUS_MSG[props.status]}
+        </span>
+      )
+    case exports.TICKET_STATUS.NEW:
+      return (
+        <span className="label label-danger">
+          {exports.TICKET_STATUS_MSG[props.status]}
+        </span>
+      )
+    case exports.TICKET_STATUS.WAITING_CUSTOMER_SERVICE:
+      return (
+        <span className="label label-warning">
+          {exports.TICKET_STATUS_MSG[props.status]}
+        </span>
+      )
+    case exports.TICKET_STATUS.WAITING_CUSTOMER:
+      return (
+        <span className="label label-primary">
+          {exports.TICKET_STATUS_MSG[props.status]}
+        </span>
+      )
+    default:
+      throw new Error('unkonwn ticket status:', props.status)
   }
 }
 exports.TicketStatusLabel.displayName = 'TicketStatusLabel'
 exports.TicketStatusLabel.propTypes = {
-  status: PropTypes.number.isRequired,
+  status: PropTypes.number.isRequired
 }
 
-exports.Avatar = (props) => {
-  let src = `https://cdn.v2ex.com/gravatar/${props.user.gravatarHash || props.user.get('gravatarHash')}?s=${props.height || 16}&r=pg&d=identicon`
-  return <Image height={props.height || 16} width={props.width || 16} src={src} rounded />
+exports.Avatar = props => {
+  let src = `https://cdn.v2ex.com/gravatar/${props.user.gravatarHash ||
+    props.user.get('gravatarHash')}?s=${props.height || 16}&r=pg&d=identicon`
+  return (
+    <Image
+      height={props.height || 16}
+      width={props.width || 16}
+      src={src}
+      rounded
+    />
+  )
 }
 exports.Avatar.displayName = 'Avatar'
 exports.Avatar.propTypes = {
@@ -165,18 +214,35 @@ exports.Avatar.propTypes = {
   width: PropTypes.string
 }
 
-exports.CategoriesSelect = ({categoriesTree, selected, onChange, hiddenDisable = true}) => {
-  const options = _.compact(exports.depthFirstSearchMap(categoriesTree, c => {
-    if (hiddenDisable && c.get('deletedAt')) {
-      return
-    }
-    return <option key={c.id} value={c.id} disabled={selected && (selected.id || selected.objectId) == c.id}>{exports.getNodeIndentString(c) + exports.getCategoryName(c)}</option>
-  }))
+exports.CategoriesSelect = ({
+  categoriesTree,
+  selected,
+  onChange,
+  hiddenDisable = true
+}) => {
+  const options = _.compact(
+    exports.depthFirstSearchMap(categoriesTree, c => {
+      if (hiddenDisable && c.get('deletedAt')) {
+        return
+      }
+      return (
+        <option
+          key={c.id}
+          value={c.id}
+          disabled={selected && (selected.id || selected.objectId) == c.id}
+        >
+          {exports.getNodeIndentString(c) + exports.getCategoryName(c)}
+        </option>
+      )
+    })
+  )
   return (
-    <FormControl componentClass='select'
-      value={selected ? (selected.id || selected.objectId) : ''}
-      onChange={onChange}>
-      <option value=''></option>
+    <FormControl
+      componentClass="select"
+      value={selected ? selected.id || selected.objectId : ''}
+      onChange={onChange}
+    >
+      <option value="" />
       {options}
     </FormControl>
   )
@@ -190,7 +256,6 @@ exports.CategoriesSelect.propTypes = {
 }
 
 exports.UserForm = class UserForm extends React.Component {
-
   constructor(props) {
     super(props)
     this.state = {
@@ -199,58 +264,77 @@ exports.UserForm = class UserForm extends React.Component {
   }
 
   handleNameChange(e) {
-    this.setState({username: e.target.value})
+    this.setState({ username: e.target.value })
   }
 
   handleSubmit(e) {
     e.preventDefault()
-    AV.Cloud.run('getUserInfo', {username: this.state.username})
-    .then(user => {
-      if (!user) {
-        throw new Error(`找不到用户名为 ${this.state.username} 的用户`)
-      }
-      return AV.Object.createWithoutData('_User', user.objectId).fetch()
-    })
-    .then(user => {
-      this.props.addUser(user)
-      this.setState({username: ''})
-      return
-    })
-    .catch(this.context.addNotification)
+    AV.Cloud.run('getUserInfo', { username: this.state.username })
+      .then(user => {
+        if (!user) {
+          throw new Error(`找不到用户名为 ${this.state.username} 的用户`)
+        }
+        return AV.Object.createWithoutData('_User', user.objectId).fetch()
+      })
+      .then(user => {
+        this.props.addUser(user)
+        this.setState({ username: '' })
+        return
+      })
+      .catch(this.context.addNotification)
   }
 
   render() {
-    return <Form inline onSubmit={this.handleSubmit.bind(this)}>
-      <FormControl type='text' value={this.state.username} onChange={this.handleNameChange.bind(this)} placeholder='用户名' />
-      {' '}
-      <Button type='submit' bsStyle='primary'>添加</Button>
-    </Form>
+    return (
+      <Form inline onSubmit={this.handleSubmit.bind(this)}>
+        <FormControl
+          type="text"
+          value={this.state.username}
+          onChange={this.handleNameChange.bind(this)}
+          placeholder="用户名"
+        />{' '}
+        <Button type="submit" bsStyle="primary">
+          添加
+        </Button>
+      </Form>
+    )
   }
 }
 exports.UserForm.propTypes = {
-  addUser: PropTypes.func,
+  addUser: PropTypes.func
 }
 exports.UserForm.contextTypes = {
-  addNotification: PropTypes.func.isRequired,
+  addNotification: PropTypes.func.isRequired
 }
 
 exports.OrganizationSelect = class OrganizationSelect extends React.Component {
-
   render() {
-    return <FormGroup controlId='orgSelect'>
-      <ControlLabel>所属：</ControlLabel>
-      <FormControl componentClass='select' value={this.props.selectedOrgId} onChange={this.props.onOrgChange}>
-        {this.props.organizations.map(o => <option key={o.id} value={o.id}>组织：{o.get('name')}</option>)}
-        <option value=''>个人：{exports.getUserDisplayName(AV.User.current())}</option>
-      </FormControl>
-    </FormGroup>
+    return (
+      <FormGroup controlId="orgSelect">
+        <ControlLabel>所属：</ControlLabel>
+        <FormControl
+          componentClass="select"
+          value={this.props.selectedOrgId}
+          onChange={this.props.onOrgChange}
+        >
+          {this.props.organizations.map(o => (
+            <option key={o.id} value={o.id}>
+              组织：{o.get('name')}
+            </option>
+          ))}
+          <option value="">
+            个人：{exports.getUserDisplayName(AV.User.current())}
+          </option>
+        </FormControl>
+      </FormGroup>
+    )
   }
 }
 
 exports.OrganizationSelect.propTypes = {
   organizations: PropTypes.array,
   selectedOrgId: PropTypes.string,
-  onOrgChange: PropTypes.func,
+  onOrgChange: PropTypes.func
 }
 
 exports.getCategoriesTree = (hiddenDisable = true) => {
@@ -258,7 +342,8 @@ exports.getCategoriesTree = (hiddenDisable = true) => {
   if (hiddenDisable) {
     query.doesNotExist('deletedAt')
   }
-  return query.descending('createdAt')
+  return query
+    .descending('createdAt')
     .find()
     .then(categories => {
       return exports.makeTree(categories)
@@ -272,14 +357,14 @@ exports.getCategoriesTree = (hiddenDisable = true) => {
     })
 }
 
-const getNodeDepth = (obj) => {
+const getNodeDepth = obj => {
   if (!obj.parent) {
     return 0
   }
   return 1 + getNodeDepth(obj.parent)
 }
 
-exports.getNodePath = (obj) => {
+exports.getNodePath = obj => {
   if (!obj.parent) {
     return [obj]
   }
@@ -288,17 +373,20 @@ exports.getNodePath = (obj) => {
   return result
 }
 
-exports.getNodeIndentString = (treeNode) => {
+exports.getNodeIndentString = treeNode => {
   const depth = getNodeDepth(treeNode)
   return depth == 0 ? '' : '　'.repeat(depth) + '└ '
 }
 
-exports.getCategoryName = (category) => {
+exports.getCategoryName = category => {
   return category.get('name') + (category.get('deletedAt') ? '（停用）' : '')
 }
 
-exports.TagForm = class TagForm extends React.Component {
+exports.isCN = () => {
+  return window.location.hostname.endsWith('.cn')
+}
 
+exports.TagForm = class TagForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -310,77 +398,108 @@ exports.TagForm = class TagForm extends React.Component {
   handleChange(e) {
     const tagMetadata = this.props.tagMetadata
     if (tagMetadata.get('type') == 'select') {
-      return this.props.changeTagValue(tagMetadata.get('key'), e.target.value, tagMetadata.get('isPrivate'))
-      .then(() => {
-        this.setState({isUpdate: false})
-        return
-      })
+      return this.props
+        .changeTagValue(
+          tagMetadata.get('key'),
+          e.target.value,
+          tagMetadata.get('isPrivate')
+        )
+        .then(() => {
+          this.setState({ isUpdate: false })
+          return
+        })
     }
 
-    this.setState({value: e.target.value})
+    this.setState({ value: e.target.value })
   }
 
   handleCommit() {
     const tagMetadata = this.props.tagMetadata
-    return this.props.changeTagValue(tagMetadata.get('key'), this.state.value, tagMetadata.get('isPrivate'))
-    .then(() => {
-      this.setState({isUpdate: false})
-      return
-    })
+    return this.props
+      .changeTagValue(
+        tagMetadata.get('key'),
+        this.state.value,
+        tagMetadata.get('isPrivate')
+      )
+      .then(() => {
+        this.setState({ isUpdate: false })
+        return
+      })
   }
 
   render() {
-    const {tagMetadata, tag, isCustomerService} = this.props
+    const { tagMetadata, tag, isCustomerService } = this.props
     const isPrivate = tagMetadata.get('isPrivate')
     if (isPrivate && !isCustomerService) {
-      return <div></div>
+      return <div />
     }
 
     // 如果标签不存在，说明标签还没设置过。对于非客服来说则什么都不显示
     if (!tag && !isCustomerService) {
-      return <div></div>
+      return <div />
     }
 
-    return <FormGroup key={tagMetadata.get('key')}>
-      <label className="label-block">
-        {tagMetadata.get('key')}
-        {' '}
-        {isPrivate && <span className='label label-default'>Private</span>}
-      </label>
-      {this.state.isUpdate ?
-        tagMetadata.get('type') == 'select' ?
-          <FormControl componentClass="select"
+    return (
+      <FormGroup key={tagMetadata.get('key')}>
+        <label className="label-block">
+          {tagMetadata.get('key')}{' '}
+          {isPrivate && <span className="label label-default">Private</span>}
+        </label>
+        {this.state.isUpdate ? (
+          tagMetadata.get('type') == 'select' ? (
+            <FormControl
+              componentClass="select"
               value={tag ? tag.value : ''}
-              onChange={this.handleChange.bind(this)}>
-            <option></option>
-            {tagMetadata.get('values').map(v => {
-              return <option key={v}>{v}</option>
-            })}
-          </FormControl>
-          :
-          <div>
-            <FormControl type='text' value={this.state.value} onChange={this.handleChange.bind(this)} />
-            <Button bsStyle='success' onClick={this.handleCommit.bind(this)}>保存</Button>
-            <Button onClick={() => this.setState({isUpdate: false})}>取消</Button>
-          </div>
-        :
-        <span>
-          {tag ?
-            validUrl.isUri(tag.value) ?
-              <a href={tag.value} target='_blank'>{tag.value}</a>
-              :
-              <span>{tag.value}</span>
-            :
-            '<未设置>'
-          }
-          {isCustomerService &&
-            <Button bsStyle='link' onClick={() => this.setState({isUpdate: true})}>
-              <span className='glyphicon glyphicon-pencil' aria-hidden="true"></span>
-            </Button>
-          }
-        </span>
-      }
-    </FormGroup>
+              onChange={this.handleChange.bind(this)}
+            >
+              <option />
+              {tagMetadata.get('values').map(v => {
+                return <option key={v}>{v}</option>
+              })}
+            </FormControl>
+          ) : (
+            <div>
+              <FormControl
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange.bind(this)}
+              />
+              <Button bsStyle="success" onClick={this.handleCommit.bind(this)}>
+                保存
+              </Button>
+              <Button onClick={() => this.setState({ isUpdate: false })}>
+                取消
+              </Button>
+            </div>
+          )
+        ) : (
+          <span>
+            {tag ? (
+              validUrl.isUri(tag.value) ? (
+                <a href={tag.value} target="_blank">
+                  {tag.value}
+                </a>
+              ) : (
+                <span>{tag.value}</span>
+              )
+            ) : (
+              '<未设置>'
+            )}
+            {isCustomerService && (
+              <Button
+                bsStyle="link"
+                onClick={() => this.setState({ isUpdate: true })}
+              >
+                <span
+                  className="glyphicon glyphicon-pencil"
+                  aria-hidden="true"
+                />
+              </Button>
+            )}
+          </span>
+        )}
+      </FormGroup>
+    )
   }
 }
 
@@ -388,5 +507,5 @@ exports.TagForm.propTypes = {
   tagMetadata: PropTypes.object.isRequired,
   tag: PropTypes.object,
   changeTagValue: PropTypes.func,
-  isCustomerService: PropTypes.bool,
+  isCustomerService: PropTypes.bool
 }
