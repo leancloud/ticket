7033698e10a85578c993ef7664d403a7c6767b05
XXu <284898541@qq.com>
2020-03-20 15:09:32 +0800

    Revert "Merge branch 'leancloud' into develop"

--- modules/Login.js
+++ modules/Login.js
@@ -1,24 +1,18 @@
 /*global ORG_NAME, USE_OAUTH, LEANCLOUD_OAUTH_REGION*/
-import React, { Component } from 'react'
-import {
-  ControlLabel,
-  FormControl,
-  FormGroup,
-  Form,
-  Button
-} from 'react-bootstrap'
+import React, {Component} from 'react'
+import {ControlLabel, FormControl, FormGroup, Form, Button} from 'react-bootstrap'
 import PropTypes from 'prop-types'
 import AV from 'leancloud-storage/live-query'
-import { isCN } from './common'
 import css from './Login.css'
 
 export default class Login extends Component {
+
   constructor(props) {
     super(props)
     this.state = {
       username: '',
       password: '',
-      error: ''
+      error: '',
     }
   }
 
@@ -26,15 +20,15 @@ export default class Login extends Component {
     const query = this.props.location.query
     if (query.token) {
       return AV.User.become(query.token)
-        .then(user => {
-          this.props.onLogin(user)
-          return
-        })
-        .then(() => {
-          this.context.router.push('/tickets')
-          return
-        })
-        .catch(this.context.addNotification)
+      .then((user) => {
+        this.props.onLogin(user)
+        return
+      })
+      .then(() => {
+        this.context.router.push('/tickets')
+        return
+      })
+      .catch(this.context.addNotification)
     }
   }
 
@@ -45,44 +39,45 @@ export default class Login extends Component {
     const query = nextProps.location.query
     if (query.token) {
       return AV.User.become(query.token)
-        .then(user => {
-          this.props.onLogin(user)
-          return
-        })
-        .then(() => {
-          this.redirect(nextProps)
-          return
-        })
-        .catch(this.context.addNotification)
+      .then((user) => {
+        this.props.onLogin(user)
+        return
+      })
+      .then(() => {
+        this.redirect(nextProps)
+        return
+      })
+      .catch(this.context.addNotification)
     }
   }
 
   handleLogin() {
     return AV.User.logIn(this.state.username, this.state.password)
-      .then(user => {
-        this.props.onLogin(user)
-        return
-      })
-      .then(() => {
-        this.redirect(this.props)
-        return
-      })
-      .catch(this.context.addNotification)
+    .then((user) => {
+      this.props.onLogin(user)
+      return
+    })
+    .then(() => {
+      this.redirect(this.props)
+      return
+    })
+    .catch(this.context.addNotification)
   }
 
   handleSignup() {
-    return AV.User.signUp(this.state.username, this.state.password, {
-      name: this.state.username
+    return AV.User.signUp(
+      this.state.username,
+      this.state.password,
+      {name: this.state.username})
+    .then((user) => {
+      this.props.onLogin(user)
+      return
     })
-      .then(user => {
-        this.props.onLogin(user)
-        return
-      })
-      .then(() => {
-        this.redirect(this.props)
-        return
-      })
-      .catch(this.context.addNotification)
+    .then(() => {
+      this.redirect(this.props)
+      return
+    })
+    .catch(this.context.addNotification)
   }
 
   redirect(props) {
@@ -95,88 +90,60 @@ export default class Login extends Component {
   }
 
   handleUsernameChange(e) {
-    this.setState({ username: e.target.value })
+    this.setState({username: e.target.value})
   }
 
   handlePasswordChange(e) {
-    this.setState({ password: e.target.value })
+    this.setState({password: e.target.value})
   }
 
   render() {
     if (USE_OAUTH === 'false') {
-      return (
-        <div className={css.wrap}>
-          <h1 className="font-logo">登录或注册</h1>
-          <hr />
-          <form>
-            <FormGroup>
-              <ControlLabel>用户名</ControlLabel>
-              <FormControl
-                type="text"
-                value={this.state.username}
-                onChange={this.handleUsernameChange.bind(this)}
-              />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>密码</ControlLabel>
-              <FormControl
-                type="password"
-                value={this.state.password}
-                onChange={this.handlePasswordChange.bind(this)}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Button
-                type="button"
-                bsStyle="primary"
-                onClick={this.handleLogin.bind(this)}
-              >
-                登录
-              </Button>{' '}
-              <Button type="button" onClick={this.handleSignup.bind(this)}>
-                注册
-              </Button>
-            </FormGroup>
-          </form>
-        </div>
-      )
+      return <div className={css.wrap}>
+        <h1 className='font-logo'>登录或注册</h1>
+        <hr />
+        <form>
+          <FormGroup>
+            <ControlLabel>用户名</ControlLabel>
+            <FormControl type='text' value={this.state.username} onChange={this.handleUsernameChange.bind(this)} />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>密码</ControlLabel>
+            <FormControl type='password' value={this.state.password} onChange={this.handlePasswordChange.bind(this)} />
+          </FormGroup>
+          <FormGroup>
+            <Button type='button' bsStyle='primary' onClick={this.handleLogin.bind(this)}>登录</Button>
+            {' '}
+            <Button type='button' onClick={this.handleSignup.bind(this)}>注册</Button>
+          </FormGroup>
+        </form>
+      </div>
     }
 
-    return (
-      <div className={css.wrap}>
-        <h1 className="font-logo">欢迎回来</h1>
-        <hr />
-        <p>目前只支持通过 {ORG_NAME} OAuth 授权进行登录</p>
-        <Form action="/oauth/login" method="post">
-          <input type="hidden" name="region" value={LEANCLOUD_OAUTH_REGION} />
-          <FormGroup>
-            <Button type="submit" bsStyle="primary">
-              前往 {ORG_NAME} 授权页
-            </Button>
-          </FormGroup>
-        </Form>
-        {isCN() && (
-          <div>
-            <hr />
-            <p>美味书签（北京）信息技术有限公司 版权所有</p>
-            <div>
-              <a href="http://www.miitbeian.gov.cn/" target="_blank">
-                京ICP备12025059号-10
-              </a>
-            </div>
-          </div>
-        )}
-      </div>
-    )
+    return <div className={css.wrap}>
+      <h1 className='font-logo'>欢迎回来</h1>
+      <hr />
+      <p>目前只支持通过 {ORG_NAME} OAuth 授权进行登录</p>
+      <Form action='/oauth/login' method='post'>
+        <input type='hidden' name='region' value={LEANCLOUD_OAUTH_REGION} />
+        <FormGroup>
+          <Button type='submit' bsStyle='primary'>前往 {ORG_NAME} 授权页</Button>
+        </FormGroup>
+      </Form>
+      <p>美味书签（北京）信息技术有限公司 版权所有</p>
+      <div><a href="http://www.miitbeian.gov.cn/" target="_blank">京ICP备12025059号-10</a></div>  
+    </div>
+    
   }
+
 }
 
 Login.propTypes = {
   location: PropTypes.object,
-  onLogin: PropTypes.func
+  onLogin: PropTypes.func,
 }
 
 Login.contextTypes = {
   router: PropTypes.object,
-  addNotification: PropTypes.func.isRequired
+  addNotification: PropTypes.func.isRequired,
 }
