1130e8dff88f9e130f92fdb6bff1068a817a898e
Jang Rush <weakish@gmail.com>
2020-10-28 18:47:53 +0800

    feat(i18n): User

--- modules/i18n/locales.js
+++ modules/i18n/locales.js
@@ -1,21 +1,31 @@
 const en = {
+  // about
   about: 'About',
   lightweight: 'A lightweight',
   oss: 'open-source', 
   intro: 'support ticket application, helping you to make happy customers.',
   builtWith: 'Built with React, Express, and ',
   leanCloudUrl: 'https://leancloud.app',
-  builtWithEnding: '.'
+  builtWithEnding: '.',
+
+  // general
+  loading: 'Loading',
+  ticketList: 'Tickets',
 }
 
 const zh = {
+  // about
   about: '关于',
   lightweight: '轻量级的',
   oss: '开源', 
   intro: '工单应用，助你提升客户满意度。',
   builtWith: '基于 React、Express、',
   leanCloudUrl: 'https://leancloud.cn',
-  builtWithEnding: '构建。'
+  builtWithEnding: '构建。',
+
+  // general
+  loading: '读取中',
+  ticketList: '工单列表',
 }
 
 const locales = {en, zh}
