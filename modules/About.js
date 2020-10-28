b5c52af6b77f99ddba81c4b99c221672a1acf85a
Jang Rush <weakish@gmail.com>
2020-10-28 18:03:06 +0800

    feat(i18n): about page

--- modules/About.js
+++ modules/About.js
@@ -5,13 +5,11 @@ import translate from './i18n/translate'
 // eslint-disable-next-line react/prop-types
 function About({ t }) {
   return <div>
-    <DocumentTitle title='关于 - LeanTicket' />
-    <h1>{t('world')}</h1>
-    <h1 className='font-logo'>LeanCloud Ticket</h1>
+    <DocumentTitle title={`${t('about')} - LeanTicket`} />
+    <h1 className='font-logo'>LeanTicket</h1>
     <hr />
-    <p>该应用是 <a href='https://leancloud.cn/'>LeanCloud</a> 的工单系统，为更有效地解决 LeanCloud 开发者的问题而创建。</p>
-    <p>该应用开放 <a href='https://github.com/leancloud/ticket'>源代码</a>，旨在帮助开发者了解 LeanCloud 各项服务的使用方法以及一些特定场景的解决方案。</p>
-    <p>您可以查看该应用的 <a href='https://github.com/leancloud/ticket/blob/master/CHANGELOG.md'>更新记录</a> 来了解最新功能和问题修复。</p>
+    <p>{t('lightweight')} <a href="https://github.com/leancloud/ticket">{t('oss')}</a> {t('intro')}.</p>
+    <p>{t('builtWith')}<a href={t('leanCloudUrl')}>LeanCloud</a>{t('builtWithEnding')}</p>
   </div>
 }
 
--- modules/i18n/locales.js
+++ modules/i18n/locales.js
@@ -1,9 +1,21 @@
 const en = {
-  world: 'Hello, World'
+  about: 'About',
+  lightweight: 'A lightweight',
+  oss: 'open-source', 
+  intro: 'support ticket application, helping you to make happy customers.',
+  builtWith: 'Built with React, Express, and ',
+  leanCloudUrl: 'https://leancloud.app',
+  builtWithEnding: '.'
 }
 
 const zh = {
-  world: '你好，世界'
+  about: '关于',
+  lightweight: '轻量级的',
+  oss: '开源', 
+  intro: '工单应用，助你提升客户满意度。',
+  builtWith: '基于 React、Express、',
+  leanCloudUrl: 'https://leancloud.cn',
+  builtWithEnding: '构建。'
 }
 
 const locales = {en, zh}
