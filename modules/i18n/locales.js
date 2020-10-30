20a7bab36fe1abdd314c859dde127eebcd85c711
Jang Rush <weakish@gmail.com>
2020-10-30 17:08:38 +0800

    feat(i18n): CSStatsUser

--- modules/i18n/locales.js
+++ modules/i18n/locales.js
@@ -1,6 +1,6 @@
 /* eslint-disable i18n/no-chinese-character */
 const en = {
-  // about
+  // About
   about: 'About',
   lightweight: 'A lightweight',
   oss: 'open-source', 
@@ -10,13 +10,22 @@ const en = {
   builtWithEnding: '.',
 
   // general
+  hour: 'hour',
   loading: 'Loading',
   preview: 'Preview',
+  statistics: 'Statistics',
+  ticket: 'Ticket',
   ticketList: 'Tickets',
+
+  // CSStatsUser
+  notInvoled: 'Not involved',
+  firstRelyTime: 'first reply time',
+  averageReplyTime: 'average reply time',
+  replyCount: 'replies',
 }
 
 const zh = {
-  // about
+  // About
   about: '关于',
   lightweight: '轻量级的',
   oss: '开源', 
@@ -26,9 +35,18 @@ const zh = {
   builtWithEnding: '构建。',
 
   // general
+  hour: '小时',
   loading: '读取中',
   preview: '预览',
+  statistics: '统计',
+  ticket: '工单',
   ticketList: '工单列表',
+
+  // CSStatsUser
+  notInvoled: '没有参与',
+  firstRelyTime: '首次回复时间',
+  averageReplyTime: '平均回复时间',
+  replyCount: '回复次数',
 }
 
 const locales = {en, zh}
