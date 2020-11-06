26fb81121c27ffb7e600f80ae12a8b34c7b89085
Jang Rush <weakish@gmail.com>
2020-11-06 14:49:47 +0800

    feat(i18n): TicketsMoveButton

--- modules/TicketsMoveButton.js
+++ modules/TicketsMoveButton.js
@@ -2,21 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
 import { DropdownButton, MenuItem } from 'react-bootstrap'
-
+import translate from './i18n/translate'
 
 const TicketsMoveButton = (props) => {
+  const {t} = props
   let items
   if (props.selectedOrgId == '') {
     items = props.organizations.map(org => {
-      return <MenuItem key={org.id} onClick={() => props.onTicketsMove(org)}>组织：{org.get('name')}</MenuItem>
+      return <MenuItem key={org.id} onClick={() => props.onTicketsMove(org)}>{t('organization')} {org.get('name')}</MenuItem>
     })
   } else {
     items = _.reject(props.organizations, {id: props.selectedOrgId}).map(org => {
-      return <MenuItem key={org.id} onClick={() => props.onTicketsMove(org)}>组织：{org.get('name')}</MenuItem>
+      return <MenuItem key={org.id} onClick={() => props.onTicketsMove(org)}>{t('organization')} {org.get('name')}</MenuItem>
     })
-    items.push(<MenuItem key={'author'} onClick={() => props.onTicketsMove()}>创建者名下</MenuItem>)
+    items.push(<MenuItem key={'author'} onClick={() => props.onTicketsMove()}>{t('itsCreator')}</MenuItem>)
   }
-  return <DropdownButton title='工单转移到' id='tickets-move'>
+  return <DropdownButton title={t('moveTicketTo')} id='tickets-move'>
       {items}
     </DropdownButton>
 }
@@ -25,6 +26,7 @@ TicketsMoveButton.propTypes = {
   organizations: PropTypes.array,
   selectedOrgId: PropTypes.string,
   onTicketsMove: PropTypes.func,
+  t: PropTypes.func
 }
 
-export default TicketsMoveButton
+export default translate(TicketsMoveButton)
