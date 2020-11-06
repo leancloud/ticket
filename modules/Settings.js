3d28154ff304d8230c70efb42d2af43c7b818f5a
Jang Rush <weakish@gmail.com>
2020-11-06 14:27:34 +0800

    feat(i18n): Settings

--- modules/Settings.js
+++ modules/Settings.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router'
+import translate from './i18n/translate'
 
-export default function Settings(props) {
+function Settings(props) {
+  const {t} = props
   return (
     <div>
       <div className="row">
         <div className="col-md-3">
           <div className="panel panel-default">
-            <div className="panel-heading">设置</div>
+            <div className="panel-heading">{t('settings')}</div>
             <ul className="list-group">
-              <li className="list-group-item"><Link to='/settings/profile'>个人设置</Link></li>
-              <li className="list-group-item"><Link to='/settings/organizations'>组织设置</Link></li>
+              <li className="list-group-item"><Link to='/settings/profile'>{t('personalSettings')}</Link></li>
+              <li className="list-group-item"><Link to='/settings/organizations'>{t('organizationSettings')}</Link></li>
             </ul>
           </div>
           {props.isCustomerService &&
             <div className="panel panel-default">
-              <div className="panel-heading">技术支持设置</div>
+              <div className="panel-heading">{t('staffSettings')}</div>
               <ul className="list-group">
-                <li className="list-group-item"><Link to='/settings/customerServiceProfile'>个人设置</Link></li>
-                <li className="list-group-item"><Link to='/settings/members'>成员</Link></li>
-                <li className="list-group-item"><Link to='/settings/categories'>分类</Link></li>
-                <li className="list-group-item"><Link to='/settings/tags'>标签</Link></li>
+                <li className="list-group-item"><Link to='/settings/customerServiceProfile'>{t('personalSettings')}</Link></li>
+                <li className="list-group-item"><Link to='/settings/members'>{t('member')}</Link></li>
+                <li className="list-group-item"><Link to='/settings/categories'>{t('category')}</Link></li>
+                <li className="list-group-item"><Link to='/settings/tags'>{t('tag')}</Link></li>
               </ul>
             </div>
           }
@@ -48,4 +50,7 @@ Settings.propTypes = {
   organizations: PropTypes.array,
   joinOrganization: PropTypes.func,
   leaveOrganization: PropTypes.func,
+  t: PropTypes.func
 }
+
+export default translate(Settings)
