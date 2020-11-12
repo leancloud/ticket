665a61f4e1fca8d421b763ce61cdcd1d363d117c
Jang Rush <weakish@gmail.com>
2020-11-12 16:42:57 +0800

    fix(OrganizationSelect): import error

--- modules/OrganizationSelect.js
+++ modules/OrganizationSelect.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {FormGroup, FormControl, ControlLabel} from 'react-bootstrap'
 import AV from 'leancloud-storage/live-query'
+import {getUserDisplayName} from './common'
 import translate from './i18n/translate'
 
 class OrganizationSelect extends React.Component {
@@ -12,7 +13,7 @@ class OrganizationSelect extends React.Component {
         <ControlLabel>{t('belong')}: </ControlLabel>
         <FormControl componentClass='select' value={this.props.selectedOrgId} onChange={this.props.onOrgChange}>
           {this.props.organizations.map(o => <option key={o.id} value={o.id}>{t('organization')}: {o.get('name')}</option>)}
-          <option value=''>{t('individual')}: {exports.getUserDisplayName(AV.User.current())}</option>
+          <option value=''>{t('individual')}: {getUserDisplayName(AV.User.current())}</option>
         </FormControl>
       </FormGroup>
   }
