3cf0c444e28b36346cc5a49f924ac1998f2c0af9
sdjdd <i@sdjdd.com>
2021-03-10 17:18:55 +0800

    feat: comments of tgb ticket custom metadata

--- config.webapp.js
+++ config.webapp.js
@@ -5,3 +5,21 @@ import { setConfig } from './modules/config'
 setConfig('stats.offsetDays', -3)
 
 setConfig('weekendWarning.enabled', true)
+
+/* eslint-disable i18n/no-chinese-character */
+setConfig('ticket.metadata.customMetadata.comments', {
+  CH: '渠道',
+  LANG: '设备语言',
+  PN: '应用版本代码',
+  UID: '设备ID',
+  VID: 'TapID',
+  VN: '应用版本号',
+  contact: '联系方式',
+  deviceName: '设备',
+  deviceVersion: '设备型号',
+  gameID: '游戏ID',
+  gameName: '用户选择的游戏名字',
+  systemVersion: '系统版本',
+  taptapID: 'TapID',
+})
+/* eslint-enable i18n/no-chinese-character */
