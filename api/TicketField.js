1524c660f7c9f884be953d0343c6cf2d6d92af30
sdjdd <i@sdjdd.com>
2021-04-23 11:55:32 +0800

    feat: modify & delete field variant

--- api/TicketField.js
+++ api/TicketField.js
@@ -6,7 +6,7 @@ const { requireAuth, customerServiceOnly, catchError } = require('./middleware')
 
 const router = Router().use(requireAuth, customerServiceOnly)
 
-const TYPES = ['dropdown', 'text', 'multi-line', 'select', 'multi-select', 'checkbox', 'numeric']
+const TYPES = ['dropdown', 'text', 'multi-line', 'multi-select', 'checkbox', 'numeric']
 const LOCALES = [
   'zh-cn',
   'zh-tw',
@@ -23,6 +23,7 @@ const LOCALES = [
   'pt',
   'tr',
 ]
+const REQUIRE_OPTIONS = ['dropdown', 'multi-select']
 
 function getVariants(id) {
   return new AV.Query('TicketFieldVariant')
@@ -37,7 +38,7 @@ router.post(
   check('type')
     .isString()
     .custom((value) => TYPES.includes(value)),
-  check('required').isBoolean().optional().default(false),
+  check('required').default(false).isBoolean(),
   check('defaultLocale')
     .isString()
     .custom((value) => LOCALES.includes(value)),
@@ -57,9 +58,9 @@ router.post(
     if (!variantLocales.includes(defaultLocale)) {
       throw new Error('Variant with default locale must be provided')
     }
-    if (type === 'select' || type === 'multi-select') {
+    if (REQUIRE_OPTIONS.includes(type)) {
       if (variants.findIndex((v) => !v.options) !== -1) {
-        throw new Error('The variants.*.options is required when type is ' + req.body.type)
+        throw new Error('The variants.*.options is required when type is ' + type)
       }
     }
 
@@ -95,7 +96,7 @@ router.post(
 router.get(
   '/',
   catchError(async (req, res) => {
-    const { page } = req.query
+    const { page, active } = req.query
     const query = new AV.Query('TicketField')
     if (page?.size) {
       const limit = parseInt(page.size)
@@ -103,28 +104,37 @@ router.get(
         query.limit(limit)
       }
     }
-    if (page?.after) {
-      query.greaterThan('objectId', page.after)
+    if (page?.skip) {
+      const skip = parseInt(page.skip)
+      if (!Number.isNaN(skip)) {
+        query.skip(skip)
+      }
+    }
+    if (active === 'true') {
+      query.equalTo('active', true)
+    }
+    if (active === 'false') {
+      query.equalTo('active', false)
     }
 
-    const objects = await query.find({ useMasterKey: true })
-    res.json(
-      objects.map((o) => ({
+    const fields = await query.find({ useMasterKey: true })
+    res.json({
+      fields: fields.map((o) => ({
         id: o.id,
         name: o.get('name'),
         type: o.get('type'),
         defaultLocale: o.get('defaultLocale'),
         active: !!o.get('active'),
         required: !!o.get('required'),
-      }))
-    )
+      })),
+    })
   })
 )
 
 router.param(
   'id',
   catchError(async (req, res, next, id) => {
-    req.object = await new AV.Query('TicketField').get(id, { useMasterKey: true })
+    req.field = await new AV.Query('TicketField').get(id, { useMasterKey: true })
     next()
   })
 )
@@ -132,14 +142,14 @@ router.param(
 router.get(
   '/:id',
   catchError(async (req, res) => {
-    const { object } = req
-    const variants = await getVariants(object.id)
+    const { field } = req
+    const variants = await getVariants(field.id)
     res.json({
-      name: object.get('name'),
-      type: object.get('type'),
-      active: !!object.get('active'),
-      required: !!object.get('required'),
-      defaultLocale: object.get('defaultLocale'),
+      name: field.get('name'),
+      type: field.get('type'),
+      active: !!field.get('active'),
+      required: !!field.get('required'),
+      defaultLocale: field.get('defaultLocale'),
       variants: variants.map((v) => ({
         locale: v.get('locale'),
         title: v.get('title'),
@@ -160,24 +170,24 @@ router.patch(
     .optional(),
   catchError(async (req, res) => {
     const { name, active, required, defaultLocale } = req.body
-    const { object } = req
+    const { field } = req
     if (name) {
-      object.set('name', name)
+      field.set('name', name)
     }
     if (active !== undefined) {
-      object.set('active', active)
+      field.set('active', active)
     }
     if (required !== undefined) {
-      object.set('required', required)
+      field.set('required', required)
     }
     if (defaultLocale) {
-      const varaiants = await getVariants(object.id)
+      const varaiants = await getVariants(field.id)
       if (varaiants.findIndex((v) => v.locale === defaultLocale) === -1) {
-        throw new Error('Variant with default locale must be provided')
+        throw new Error(`No such variant with locale "${defaultLocale}"`)
       }
-      object.set('defaultLocale', defaultLocale)
+      field.set('defaultLocale', defaultLocale)
     }
-    await object.save(null, { useMasterKey: true })
+    await field.save(null, { useMasterKey: true })
     res.json({})
   })
 )
@@ -191,21 +201,70 @@ router.post(
   check('options').isArray().optional(),
   check('options.*').isString(),
   catchError(async (req, res) => {
-    const { object } = req
-    const type = object.get('type')
+    const { field } = req
+    const type = field.get('type')
     const { title, locale, options } = req.body
-    if (type === 'select' || type === 'multi-select') {
+    if (REQUIRE_OPTIONS.includes(type)) {
       if (!options) {
         throw new Error('The options is required when type is ' + type)
       }
     }
     await new AV.Object('TicketFieldVariant', {
       ACL: {},
-      field: object,
+      field,
       title,
       locale,
       options,
     }).save(null, { useMasterKey: true })
+    res.json({})
+  })
+)
+
+router.param(
+  'locale',
+  catchError(async (req, res, next, locale) => {
+    if (!LOCALES.includes(locale)) {
+      throw new Error('Unsupported locale')
+    }
+    const variant = await new AV.Query('TicketFieldVariant')
+      .equalTo('field', req.field)
+      .equalTo('locale', locale)
+      .first({ useMasterKey: true })
+    if (!variant) {
+      res.throw(404, 'Field variant not found')
+    }
+    req.variant = variant
+    next()
+  })
+)
+
+router.patch(
+  '/:id/variants/:locale',
+  check('title').isString().isLength({ min: 1 }).optional(),
+  check('options').isArray().optional(),
+  check('options.*').isString(),
+  catchError(async (req, res) => {
+    const { title, options } = req.body
+    const { field, variant } = req
+    if (title) {
+      variant.set('title', title)
+    }
+    if (REQUIRE_OPTIONS.includes(field.get('type')) && options) {
+      variant.set('options', options)
+    }
+    await variant.save(null, { useMasterKey: true })
+    res.json({})
+  })
+)
+
+router.delete(
+  '/:id/variants/:locale',
+  catchError(async (req, res) => {
+    const { field, variant } = req
+    if (variant.get('locale') === field.get('defaultLocale')) {
+      throw new Error('Cannot delete variant of default locale')
+    }
+    await variant.destroy({ useMasterKey: true })
     res.json({})
   })
 )
