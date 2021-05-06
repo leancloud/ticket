d2fff8609ee569ce8507645b97caf91543ec581f
sdjdd <i@sdjdd.com>
2021-05-06 13:28:38 +0800

    finish evaluation

--- modules/Ticket/Evaluation.js
+++ modules/Ticket/Evaluation.js
@@ -1,112 +1,81 @@
-import React, { Component } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Alert, Button, Form } from 'react-bootstrap'
-import { withTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import PropTypes from 'prop-types'
 import * as Icon from 'react-bootstrap-icons'
 
-class Evaluation extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isAlreadyEvaluation: false,
-      star: 1,
-      content: localStorage.getItem(`ticket:${this.props.ticket.objectId}:evaluation`) || '',
-    }
-  }
+import { AppContext } from '../context'
+import { fetch } from '../../lib/leancloud'
 
-  handleStarChange(e) {
-    this.setState({ star: parseInt(e.target.value) })
-  }
+export default function Evaluation({ ticket, isCustomerService }) {
+  const { t } = useTranslation()
+  const { addNotification } = useContext(AppContext)
+  const [star, setStar] = useState(ticket.evaluation?.star ?? 1)
+  const [content, setContent] = useState(ticket.evaluation?.content ?? '')
+  useEffect(() => {
+    localStorage.setItem(`ticket:${ticket.nid}:evaluation`, content)
+  }, [ticket.nid, content])
 
-  handleContentChange(e) {
-    localStorage.setItem(`ticket:${this.props.ticket.objectId}:evaluation`, e.target.value)
-    this.setState({ content: e.target.value })
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    this.props
-      .saveEvaluation({
-        star: this.state.star,
-        content: this.state.content,
+    try {
+      await fetch(`/api/1/tickets/${ticket.nid}/evaluation`, {
+        method: 'PUT',
+        body: { star, content },
       })
-      .then(() => {
-        localStorage.removeItem(`ticket:${this.props.ticket.objectId}:evaluation`)
-        return
-      })
-      .catch(this.context.addNotification)
+      localStorage.removeItem(`ticket:${ticket.nid}:evaluation`)
+    } catch (error) {
+      addNotification(error)
+    }
   }
 
-  render() {
-    const { t } = this.props
-    const { evaluation } = this.props.ticket
-    if (evaluation) {
-      return (
-        <Alert variant="warning">
-          {t('feedback')}
-          <Form.Group>
-            <Form.Check
-              type="radio"
-              inline
-              disabled
-              defaultChecked={evaluation.star === 1}
-              label={<Icon.HandThumbsUp />}
-            />
-            <Form.Check
-              type="radio"
-              inline
-              disabled
-              defaultChecked={evaluation.star === 0}
-              label={<Icon.HandThumbsDown />}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Control as="textarea" rows="8" value={evaluation.content} disabled />
-          </Form.Group>
-        </Alert>
-      )
-    }
-
-    if (!this.props.isCustomerService) {
-      return (
-        <Alert variant="warning">
-          {t('satisfiedOrNot')}
-          <Form onSubmit={this.handleSubmit.bind(this)}>
-            <Form.Group>
-              <Form.Check
-                type="radio"
-                inline
-                value="1"
-                onClick={this.handleStarChange.bind(this)}
-                label={<Icon.HandThumbsUp />}
-              />
-              <Form.Check
-                type="radio"
-                inline
-                value="0"
-                onClick={this.handleStarChange.bind(this)}
-                label={<Icon.HandThumbsDown />}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Control
-                as="textarea"
-                placeholder={t('haveSomethingToSay')}
-                rows="8"
-                value={this.state.content}
-                onChange={this.handleContentChange.bind(this)}
-              />
-            </Form.Group>
-            <Button type="submit" variant="light">
-              {t('submit')}
-            </Button>
-          </Form>
-        </Alert>
-      )
-    }
-
+  if (!ticket.evaluation && isCustomerService) {
     return null
   }
+  return (
+    <Alert variant="warning">
+      {ticket.evaluation ? t('feedback') : t('satisfiedOrNot')}
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Check
+            id="evaluation-good"
+            label={<Icon.HandThumbsUp />}
+            type="radio"
+            inline
+            disabled={!!ticket.evaluation}
+            checked={star === 1}
+            value={1}
+            onChange={() => setStar(1)}
+          />
+          <Form.Check
+            id="evaluation-bad"
+            label={<Icon.HandThumbsDown />}
+            type="radio"
+            inline
+            disabled={!!ticket.evaluation}
+            checked={star === 0}
+            value={0}
+            onChange={() => setStar(0)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            as="textarea"
+            rows={8}
+            placeholder={ticket.evaluation ? '' : t('haveSomethingToSay')}
+            value={content}
+            disabled={!!ticket.evaluation}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </Form.Group>
+        {!ticket.evaluation && (
+          <Button type="submit" variant="light">
+            {t('submit')}
+          </Button>
+        )}
+      </Form>
+    </Alert>
+  )
 }
 
 Evaluation.propTypes = {
@@ -118,12 +87,4 @@ Evaluation.propTypes = {
     }),
   }).isRequired,
   isCustomerService: PropTypes.bool,
-  saveEvaluation: PropTypes.func.isRequired,
-  t: PropTypes.func,
 }
-
-Evaluation.contextTypes = {
-  addNotification: PropTypes.func.isRequired,
-}
-
-export default withTranslation()(Evaluation)
