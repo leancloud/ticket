95f856e25b1b6e5bb88a0a9dba27d6973e3ea148
sdjdd <i@sdjdd.com>
2021-05-13 18:07:59 +0800

    add currentUser to AppContext

--- modules/context/index.js
+++ modules/context/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import _ from 'lodash'
 
 export const AppContext = React.createContext({
+  currentUser: null,
   isCustomerService: false,
   tagMetadatas: [],
   addNotification: _.noop,
