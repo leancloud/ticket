4c45322fb8266b07f755c1fb0ad35e4035b81c29
sdjdd <i@sdjdd.com>
2021-05-13 18:07:12 +0800

    use react-query

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import App from './modules/App'
 import './config.webapp'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
   document.getElementById('app')
 )
