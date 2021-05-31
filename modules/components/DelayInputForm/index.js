0c0af68bb3ce92250c35b4a92ca859d2faf64f6c
CHiCO <i@sdjdd.com>
2021-05-31 18:45:00 +0800

    refactor: DelayInputValue (#287)

--- modules/components/DelayInputForm/index.js
+++ modules/components/DelayInputForm/index.js
@@ -1,43 +1,31 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import { Form } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
 /**
  * @param {object} props
- * @param {string} [props.value]
+ * @param {string} [props.initValue]
  * @param {Function} [props.onChange]
  * @param {number} [props.delay]
  */
-export function DelayInputForm({ value = '', onChange, delay = 1000, ...props }) {
-  const [debouncedValue, setDebouncedValue] = useState(value)
-
+export function DelayInputForm({ initValue, onChange, delay, ...props }) {
+  const [value, setValue] = useState(initValue || '')
+  const $delay = useRef(delay ?? 1000)
   const $onChange = useRef(onChange)
-  useEffect(() => {
-    $onChange.current = onChange
-  }, [onChange])
-  const $delay = useRef(delay)
-  useEffect(() => {
-    $delay.current = delay
-  }, [delay])
+  $onChange.current = onChange
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      $onChange.current?.(debouncedValue)
-    }, $delay.current)
-    return () => clearTimeout(timer)
-  }, [debouncedValue])
+  const $timer = useRef()
+  const handleChange = useCallback((e) => {
+    const value = e.target.value
+    setValue(value)
+    clearTimeout($timer.current)
+    $timer.current = setTimeout(() => $onChange.current?.(value), $delay.current)
+  }, [])
 
-  const handleChange = useCallback(
-    (e) => {
-      setDebouncedValue(e.target.value)
-    },
-    [setDebouncedValue]
-  )
-
-  return <Form.Control {...props} value={debouncedValue} onChange={handleChange} />
+  return <Form.Control {...props} value={value} onChange={handleChange} />
 }
 DelayInputForm.propTypes = {
-  value: PropTypes.string,
+  initValue: PropTypes.string,
   onChange: PropTypes.func,
   delay: PropTypes.number,
 }
