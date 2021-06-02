116e27257ac629e1a31c7911093ac673b94dfaa3
sdjdd <i@sdjdd.com>
2021-06-02 11:16:45 +0800

    feat: update comments of custom metadata

--- config.webapp.js
+++ config.webapp.js
@@ -8,19 +8,8 @@ setConfig('weekendWarning.enabled', true)
 
 /* eslint-disable i18n/no-chinese-character */
 setConfig('ticket.metadata.customMetadata.comments', {
-  CH: '渠道',
-  LANG: '设备语言',
-  PN: '应用版本代码',
-  UID: '设备ID',
-  VID: 'TapID',
-  VN: '应用版本号',
-  contact: '联系方式',
-  deviceName: '设备',
-  deviceVersion: '设备型号',
-  gameID: '游戏ID',
-  gameName: '用户选择的游戏名字',
-  systemVersion: '系统版本',
-  taptapID: 'TapID',
+  game_name: '游戏名称',
+  developer_name: '厂商名称',
 })
 /* eslint-enable i18n/no-chinese-character */
 
