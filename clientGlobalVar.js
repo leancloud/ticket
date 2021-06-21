428ac7529e33034402a16a22b355a5188c386eff
CHiCO <i@sdjdd.com>
2021-06-21 15:04:32 +0800

    feat: custom gravatar url (#297)

--- clientGlobalVar.js
+++ clientGlobalVar.js
@@ -2,6 +2,10 @@ const _ = require('lodash')
 
 const clientGlobalVars = {}
 
+/**
+ * @param {string} key
+ * @param {any} value
+ */
 function setClientGlobalVar(key, value) {
   if (clientGlobalVars[key] && typeof clientGlobalVars[key] === 'object') {
     _.merge(clientGlobalVars[key], value)
@@ -10,6 +14,9 @@ function setClientGlobalVar(key, value) {
   }
 }
 
+/**
+ * @param {Record<string, any>} object
+ */
 function setClientGlobalVars(object) {
   _.merge(clientGlobalVars, object)
 }
