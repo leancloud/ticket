ce8c406914c253b7d598963a5320e66f215882c0
sdjdd <i@sdjdd.com>
2021-06-23 11:02:36 +0800

    feat: init embed v1

---
+++ embed/v1/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactJSX from 'vite-react-jsx'
+
+export default defineConfig({
+  plugins: [reactRefresh(), reactJSX()],
+  server: {
+    port: 8080,
+    // proxy: {
+    //   '/api': 'http://127.0.0.1:3000',
+    //   '/env.js': 'http://127.0.0.1:3000',
+    // },
+  },
+})
