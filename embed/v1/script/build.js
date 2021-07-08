06be5853de9e18a3861f81117c37596d70ab5032
sdjdd <i@sdjdd.com>
2021-07-08 10:49:36 +0800

    build: build embed pages at build phase

--- embed/v1/script/build.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const { exec } = require('child_process');
-
-if (!fs.existsSync(path.resolve(__dirname, '../dist'))) {
-  require('./generate_env');
-  process.chdir(path.resolve(__dirname, '..'));
-  exec('npm run build');
-}
