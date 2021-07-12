d1d5c07ada91e489f800334ff6e868cf39c59cde
Lee Yeh <lee55962698@gmail.com>
2021-07-12 20:53:34 +0800

    feat: Ticket.categoryPath

--- modules/Ticket/TicketMetadata.js
+++ modules/Ticket/TicketMetadata.js
@@ -220,7 +220,7 @@ function CategorySection({ ticket, isCustomerService }) {
         />
       ) : (
         <div className="d-flex align-items-center">
-          <Category block categoryPath={ticket.category_path} />
+          <Category block categoryId={ticket.category_id} />
           {isCustomerService && (
             <Button variant="link" onClick={() => setEditingCategory(true)}>
               <Icon.PencilFill />
@@ -235,7 +235,6 @@ CategorySection.propTypes = {
   ticket: PropTypes.shape({
     id: PropTypes.string.isRequired,
     category_id: PropTypes.string.isRequired,
-    category_path: PropTypes.array.isRequired,
   }),
   isCustomerService: PropTypes.bool,
 }
