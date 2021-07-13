e02bd7d0927aaea5db07f4742e4afb627f19d0a2
Lee Yeh <lee55962698@gmail.com>
2021-07-13 19:47:10 +0800

    feat: OAuth login via URL hash

--- embed/v1/src/App/index.tsx
+++ embed/v1/src/App/index.tsx
@@ -2,7 +2,7 @@ import { createContext, PropsWithChildren, useContext, useEffect, useMemo, useSt
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-import { auth } from 'leancloud';
+import { auth as lcAuth } from 'leancloud';
 import { ControlButton } from 'components/ControlButton';
 import { ErrorBoundary } from 'components/ErrorBoundary';
 import LogIn from './LogIn';
@@ -10,7 +10,10 @@ import Home from './Home';
 import Categories from './Categories';
 import Tickets from './Tickets';
 import { parse } from 'query-string';
-import { decodeQueryParams, JsonParam } from 'serialize-query-params';
+import { ArrayParam, decodeQueryParams, JsonParam } from 'serialize-query-params';
+import { User } from 'open-leancloud-storage/auth';
+import { Loading } from 'components/Loading';
+import { APIError } from 'components/APIError';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,7 +25,14 @@ const queryClient = new QueryClient({
 });
 
 function PrivateRoute(props: PropsWithChildren<{ path: string; exact?: boolean }>) {
-  if (!auth.currentUser) {
+  const [auth, loading, error] = useAuth();
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <APIError error={error} />;
+  }
+  if (!auth) {
     return <Redirect to="/login" />;
   }
   return <Route {...props} />;
@@ -35,21 +45,63 @@ const ROOT_URL = '/embed/v1/catogeries';
 
 const NotFound = () => <>NOT FOUND</>;
 
+const TicketInfoContext = createContext<{
+  meta?: Record<string, unknown> | null;
+  tags?: Array<string | null> | null;
+  // fields
+}>({});
+export const useTicketContext = () => useContext(TicketInfoContext);
+
+const AuthContext = createContext<[User | null, boolean, any]>([null, true, null]);
+export const useAuth = () => useContext(AuthContext);
+
 export default function App() {
   const pathname = window.location.pathname;
   if (!pathname.startsWith(ROOT_URL)) return <NotFound />;
   const paths = pathname.split('/');
   const rootCategory = paths[4] === '-' ? undefined : paths[4];
 
+  const params = useMemo(
+    () =>
+      decodeQueryParams(
+        { meta: JsonParam, tags: ArrayParam, auth: JsonParam },
+        parse(window.location.hash)
+      ),
+    []
+  );
+  const ticketInfo = useMemo(() => ({ meta: params.meta, tags: params.tags }), [params]);
+
+  const [auth, setAuth] = useState<[User | null, boolean, any]>([null, true, null]);
+  useEffect(() => {
+    if (params.auth) {
+      if (!params.auth['platform']) {
+        setAuth([null, false, new Error('Malformed auth param: platform is required')]);
+        return;
+      }
+      lcAuth
+        .loginWithAuthData(params.auth.platform, params.auth.data)
+        .then((user) => setAuth([user, false, null]))
+        .catch((error) => setAuth([null, false, error]));
+    } else if (lcAuth.currentUser) {
+      setAuth([lcAuth.currentUser, false, null]);
+    } else {
+      setAuth([null, false, null]);
+    }
+  }, []);
+
   return (
     <BrowserRouter basename={`${ROOT_URL}/${paths[4]}`}>
       <QueryClientProvider client={queryClient}>
         <ErrorBoundary>
           <RootCategoryContext.Provider value={rootCategory}>
-            <div className="h-full p-4 sm:px-24 pt-14 sm:pt-4">
-              <ControlButton />
-              <Routes />
-            </div>
+            <AuthContext.Provider value={auth}>
+              <TicketInfoContext.Provider value={ticketInfo}>
+                <div className="h-full p-4 sm:px-24 pt-14 sm:pt-4">
+                  <ControlButton />
+                  <Routes />
+                </div>
+              </TicketInfoContext.Provider>
+            </AuthContext.Provider>
           </RootCategoryContext.Provider>
         </ErrorBoundary>
       </QueryClientProvider>
@@ -57,37 +109,27 @@ export default function App() {
   );
 }
 
-const TicketContext = createContext<object>({});
-export const useTicketContext = () => useContext(TicketContext);
-
 const Routes = () => {
-  const ticketContext = useMemo(
-    () => decodeQueryParams({ meta: JsonParam, auth: JsonParam }, parse(window.location.hash)),
-    []
-  );
-
   return (
-    <TicketContext.Provider value={ticketContext}>
-      <Switch>
-        <Route path="/login">
-          <LogIn />
-        </Route>
-        <PrivateRoute path="/tickets">
-          <Tickets />
-        </PrivateRoute>
-        <PrivateRoute path="/categories/:id">
-          <Categories />
-        </PrivateRoute>
-        <PrivateRoute path="/tickets">
-          <Tickets />
-        </PrivateRoute>
-        <PrivateRoute path="/" exact>
-          <Home />
-        </PrivateRoute>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </TicketContext.Provider>
+    <Switch>
+      <Route path="/login">
+        <LogIn />
+      </Route>
+      <PrivateRoute path="/tickets">
+        <Tickets />
+      </PrivateRoute>
+      <PrivateRoute path="/categories/:id">
+        <Categories />
+      </PrivateRoute>
+      <PrivateRoute path="/tickets">
+        <Tickets />
+      </PrivateRoute>
+      <PrivateRoute path="/" exact>
+        <Home />
+      </PrivateRoute>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
   );
 };
