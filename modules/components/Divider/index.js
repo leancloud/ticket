e8ea6b1563c6e2b6cad1aa2ac47eee67221efab4
lywTYH <763613923@qq.com>
2021-07-30 16:16:11 +0800

    add inlineDivider

--- modules/components/Divider/index.js
+++ modules/components/Divider/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styles from './index.css'
-export default function Divider() {
-  return <span className={styles.divider} />
+export default function Divider({ inline }) {
+  return <span className={inline ? styles.inlineDivider : styles.divider} />
+}
+
+Divider.propTypes = {
+  inline: PropTypes.bool,
 }
