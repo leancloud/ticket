ba055a2e6c47148ec6a3c49d09038a2450da7ddf
Lee Yeh <lee55962698@gmail.com>
2021-08-10 21:10:32 +0800

    fix: use RedisStore for session

--- oauth/index.js
+++ oauth/index.js
@@ -1,7 +1,11 @@
 const { Router } = require('express')
 const passport = require('passport')
 const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+const Redis = require('ioredis')
 const config = require('../config')
+
+const redisClient = new Redis(process.env.REDIS_URL_CACHE)
 
 // LeanCloud OAuth
 const loginCallbackPath = '/oauth/callback'
@@ -15,7 +19,7 @@ passport.serializeUser(function (user, done) {
   done(null, user.id)
 })
 router.use(passport.initialize())
-router.use(session({ secret: 'ticket' }))
+router.use(session({ secret: 'ticket', store: new RedisStore({ client: redisClient }) }))
 router.use(passport.session()) // requied for state store
 
 if (process.env.ENABLE_XD_OAUTH) {
