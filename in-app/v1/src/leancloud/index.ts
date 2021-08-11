4fa1c604c632c21085f9b5a1f453e44cf89309a8
sdjdd <i@sdjdd.com>
2021-08-11 18:17:19 +0800

    :wise-me:

--- in-app/v1/src/leancloud/index.ts
+++ in-app/v1/src/leancloud/index.ts
@@ -1,22 +1,27 @@
-import { init, use, setAdapters } from 'open-leancloud-storage/core';
+import { init, use } from 'open-leancloud-storage/core';
 import { authModule } from 'open-leancloud-storage/auth';
 import { storageModule } from 'open-leancloud-storage/storage';
 import axios from 'axios';
 
 export type { User } from 'open-leancloud-storage/auth';
 
-setAdapters({
-  storage: {
-    async: false,
-    getItem: (key: string) => null,
-    setItem: (key: string, value: string) => undefined,
-    removeItem: (key: string) => undefined,
-    clear: () => undefined,
-  },
-});
-
 use(authModule);
 use(storageModule);
+use({
+  name: 'do-not-persist-current-user',
+  onLoad: ({ adapters }) => {
+    if (!adapters.storage) {
+      throw new Error('No storage adapter, you should set adapters first.');
+    }
+    const setItem = adapters.storage.setItem;
+    adapters.storage.setItem = (key, value) => {
+      if (key.endsWith('current_user')) {
+        return;
+      }
+      return setItem.call(adapters.storage, key, value);
+    };
+  },
+});
 
 export const app = init({
   appId: import.meta.env.VITE_LC_APP_ID,
