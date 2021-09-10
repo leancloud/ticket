d86ea39851c698921e663f75906bcf8823f105c0
sdjdd <i@sdjdd.com>
2021-09-10 16:25:41 +0800

    feat: 给 Group 添加一些字段

--- next/api/src/model2/Group.ts
+++ next/api/src/model2/Group.ts
@@ -1,3 +1,16 @@
-import { Model } from '../orm';
+import { Model, field, pointTo, pointerId } from '../orm';
+import { Role } from './Role';
 
-export class Group extends Model {}
+export class Group extends Model {
+  @field()
+  name!: string;
+
+  @field()
+  description?: string;
+
+  @pointerId(Role)
+  roleId!: string;
+
+  @pointTo(Role)
+  role!: Role;
+}
