7b3caefbe725577b50687061a1bb9030e8e65b08
sdjdd <i@sdjdd.com>
2021-09-13 18:53:38 +0800

    chore: 移除旧的 models

--- next/api/src/model/ticket-form/utils.ts
+++
@@ -1,7 +0,0 @@
-export function getAvailableLocales(locale: string): string[] {
-  const index = locale.indexOf('-');
-  if (index > 0) {
-    return [locale, locale.slice(0, index)];
-  }
-  return [locale];
-}
