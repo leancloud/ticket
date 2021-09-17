21bf268033cf9b4f70f0380d045dc9e88d1d4298
lywTYH <763613923@qq.com>
2021-09-17 19:00:11 +0800

    fix

--- modules/Ticket/RecentTickets.js
+++ modules/Ticket/RecentTickets.js
@@ -2,6 +2,7 @@ import React, { useMemo } from 'react'
 import { useQuery } from 'react-query'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import moment from 'moment'
 import { useAppContext } from 'modules/context'
 import { http } from '../../lib/leancloud'
 import styles from './index.css'
@@ -19,7 +20,7 @@ function RecentTickets({ ticket }) {
       http.get('/api/1/tickets', {
         params: {
           author_id,
-          page_size: 11,
+          page_size: 110,
           q: 'sort:created_at-desc',
           // created_at_lt: ticket.created_at,
         },
@@ -41,19 +42,30 @@ function RecentTickets({ ticket }) {
       <div className={styles.title}>{t('ticket.recently')}</div>
       <ul className={styles.recentList}>
         {tickets.map((ticketItem) => {
+          console.log(ticketItem)
           return (
             <li key={ticketItem.nid}>
-              <span>
+              <div className={styles.ticketInfo}>
                 <Link
                   to={`/tickets/${ticketItem.nid}`}
                   title={ticketItem.title}
                   className={styles.link}
                 >
+                  <span className={styles.nid}>#{ticketItem.nid}</span>
                   {ticketItem.title}
                 </Link>
                 <TicketStatusLabel status={ticketItem.status} />
+                <span
+                  className={styles.creator}
+                  title={moment(ticketItem.created_at).format('YYYY-MM-DD HH:MM')}
+                >
+                  {t('createdAt')} {moment(ticketItem.created_at).fromNow()}
+                </span>
+              </div>
+
+              <span>
+                {ticketItem.assignee ? <UserLabel user={ticketItem.assignee} /> : '<unset>'}
               </span>
-              <span>{ticket.assignee ? <UserLabel user={ticket.assignee} /> : '<unset>'}</span>
             </li>
           )
         })}
