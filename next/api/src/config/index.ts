560dcfcb24f8ccf57a5f3e1055df2bcf58d31eba
sdjdd <i@sdjdd.com>
2021-09-28 11:38:43 +0800

    refactor: config

--- next/api/src/config/index.ts
+++ next/api/src/config/index.ts
@@ -1,6 +1,33 @@
 import { Config } from '../model/Config';
 
-const host = (() => {
+export const config = {
+  allowModifyEvaluation: boolean(process.env.ALLOW_MUTATE_EVALUATION),
+  enableLeanCloudIntegration: boolean(process.env.ENABLE_LEANCLOUD_INTEGRATION),
+  gravatarURL: 'https://www.gravatar.com/avatar',
+  host: getHost(),
+  sla: 120,
+};
+
+function boolean(value: any): boolean {
+  switch (typeof value) {
+    case 'boolean':
+      return value;
+    case 'number':
+    case 'bigint':
+      return value !== 0;
+    case 'object':
+      return true;
+    case 'string':
+      if (value === '0' || value.toLowerCase() === 'false') {
+        return false;
+      }
+      return true;
+    default:
+      return false;
+  }
+}
+
+function getHost() {
   switch (process.env.LEANCLOUD_APP_ENV) {
     case 'production':
       return process.env.TICKET_HOST;
@@ -9,24 +36,16 @@ const host = (() => {
     default:
       return 'http://localhost:' + process.env.LEANCLOUD_APP_PORT;
   }
-})();
-
-export const config = {
-  host,
-  gravatarURL: 'https://www.gravatar.com/avatar',
-  enableLeanCloudIntegration: !!process.env.ENABLE_LEANCLOUD_INTEGRATION,
-  sla: 120,
-  allowModifyEvaluation: !!process.env.ALLOW_MUTATE_EVALUATION,
-};
+}
 
 Config.get('gravatar_url').then((value) => {
-  if (value) {
-    config.gravatarURL = value;
-  }
+  if (value) config.gravatarURL = value;
 });
 
 Config.get('SLA_in_mimutes').then((sla) => {
-  if (sla) {
-    config.sla = sla;
-  }
+  if (sla) config.sla = sla;
 });
+
+if (config.enableLeanCloudIntegration) {
+  console.log('[LeanCloud] Integration enabled');
+}
