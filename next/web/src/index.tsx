9edc6f5900895eccfa20f6666ce690d97660e3ab
lywTYH <763613923@qq.com>
2021-10-22 18:00:43 +0800

    add Sentry

--- next/web/src/index.tsx
+++ next/web/src/index.tsx
@@ -1,9 +1,27 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
-import 'antd/dist/antd.css';
 
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+import 'components/antd/index.less';
 import './index.css';
 import App from './App';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DNS,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+  ignoreErrors: [],
+  // release: `` ,
+  // environment: '', // 环境指代 发布的应用
+  enabled: !import.meta.env.DEV,
+  // initialScope:{
+  //   // tags: {
+  //   //     [key: string]: Primitive;
+  //   // };
+  // }
+});
 
 render(
   <StrictMode>
