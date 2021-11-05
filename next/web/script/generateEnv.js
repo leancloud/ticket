fff38829658d59a63f7883a7168217b69c6ecaf7
lyw <763613923@qq.com>
2021-11-05 15:55:27 +0800

    fix: word spelling error (#414)

--- next/web/script/generateEnv.js
+++ next/web/script/generateEnv.js
@@ -5,9 +5,9 @@ if (process.env.LEANCLOUD_APP_ID) {
   let content = `VITE_LC_APP_ID=${process.env.LEANCLOUD_APP_ID}
 VITE_LC_APP_KEY=${process.env.LEANCLOUD_APP_KEY}
 VITE_LC_API_SERVER=${process.env.LEANCLOUD_API_HOST}`;
-  content = process.env.SENTRY_WEB_DSN
+  content = process.env.VITE_SENTRY_DNS
     ? `${content}
-VITE_SENTRY_DSN=${process.env.SENTRY_WEB_DSN}`
+  VITE_SENTRY_DNS=${process.env.VITE_SENTRY_DNS}`
     : content;
 
   const filePath = path.resolve(__dirname, '../.env.local');
