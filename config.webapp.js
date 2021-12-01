aa9b1671f2d23a2dd47e132eeec6b726fe2c31af
leeyeh <lee55962698@gmail.com>
2021-12-01 12:48:06 +0800

    chore: update the default value for stats.offsetDays config to 0

--- config.webapp.js
+++ config.webapp.js
@@ -2,6 +2,6 @@ import { setConfig } from './modules/config'
 
 // Used in CustomerServiceStats.
 // 0/-1/-2/...: a week ends at 23:59:59 Sunday/Saturday/Friday/...
-setConfig('stats.offsetDays', -3)
+setConfig('stats.offsetDays', 0)
 
 setConfig('weekendWarning.enabled', true)
