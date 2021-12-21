c4074e3ad9bad2e3bfa500c1d8b9dcc19a7077e8
Ayumi Sarah <i@sdjdd.com>
2021-12-21 14:44:13 +0800

    refactor(next/web): generateEnv script (#475)

--- next/web/src/global.d.ts
+++ next/web/src/global.d.ts
@@ -4,6 +4,6 @@ interface ImportMetaEnv {
   VITE_LC_APP_ID: string;
   VITE_LC_APP_KEY: string;
   VITE_LC_API_SERVER: string;
-  VITE_SENTRY_DSN: string;
+  VITE_SENTRY_WEB_DSN: string;
   VITE_ENABLE_LEANCLOUD_INTEGRATION: string;
 }
