e57edd009169641d2a8ed84f5a8a38405b07538c
Ayumi Sarah <i@sdjdd.com>
2022-01-05 09:52:19 +0800

    feat(next/api): queues for stage environment (#485)

--- next/api/src/queue/index.ts
+++ next/api/src/queue/index.ts
@@ -3,5 +3,8 @@ import Bull from 'bull';
 const QUEUE_REDIS = process.env.REDIS_URL_QUEUE ?? 'redis://127.0.0.1:6379';
 
 export function createQueue<T>(name: string, options?: Bull.QueueOptions): Bull.Queue<T> {
+  if (process.env.NODE_ENV === 'stage') {
+    name += '_stg';
+  }
   return new Bull<T>(name, QUEUE_REDIS, options);
 }
