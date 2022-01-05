584fbaa0035934665f961a21d63b516b77b0a058
sdjdd <i@sdjdd.com>
2022-01-05 10:03:02 +0800

    fix(next/api): test value of NODE_ENV

--- next/api/src/queue/index.ts
+++ next/api/src/queue/index.ts
@@ -3,7 +3,7 @@ import Bull from 'bull';
 const QUEUE_REDIS = process.env.REDIS_URL_QUEUE ?? 'redis://127.0.0.1:6379';
 
 export function createQueue<T>(name: string, options?: Bull.QueueOptions): Bull.Queue<T> {
-  if (process.env.NODE_ENV === 'stage') {
+  if (process.env.NODE_ENV === 'staging') {
     name += '_stg';
   }
   return new Bull<T>(name, QUEUE_REDIS, options);
