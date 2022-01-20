795a9f57c1719cf895e9efb494d1d8287d5b0bce
sdjdd <i@sdjdd.com>
2022-01-20 19:03:58 +0800

    feat: next ticket form

---
+++ modules/NewTicket.next.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useRef } from 'react'
+import { useHistory } from 'react-router-dom'
+
+import './NewTicket.next.css'
+
+export default function NewTicket() {
+  const history = useRef(useHistory())
+  const frame = useRef(null)
+
+  useEffect(() => {
+    const h = ({ data }) => {
+      if (data === 'ticketCreated') {
+        history.current.push('/tickets')
+      }
+    }
+    frame.current.contentWindow.addEventListener('message', h)
+  }, [])
+
+  return <iframe ref={frame} frameBorder={0} src="/next/tickets/new" width="100%" height="100%" />
+}
