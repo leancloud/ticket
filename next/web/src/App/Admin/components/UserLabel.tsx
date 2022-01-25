c5dee2297b7ce83534753060ad67d210dbab0221
sdjdd <i@sdjdd.com>
2022-01-25 11:03:39 +0800

    feat: UserLabel component

---
+++ next/web/src/App/Admin/components/UserLabel.tsx
@@ -0,0 +1,18 @@
+import cx from 'classnames';
+
+export interface UserLabelProps {
+  className?: string;
+  user: {
+    nickname: string;
+    avatarUrl: string;
+  };
+}
+
+export function UserLabel({ className, user }: UserLabelProps) {
+  return (
+    <div className={cx('flex items-center', className)}>
+      <img className="w-4 h-4 rounded-sm" src={user.avatarUrl} />
+      <div className="ml-1">{user.nickname}</div>
+    </div>
+  );
+}
