80d9c753c9601b5c5118a14b70ad16d9dff2c5c4
Ayumi Sarah <i@sdjdd.com>
2022-01-25 14:26:31 +0800

    fix(next/web): use LEANCLOUD_API_HOST as server URL (#512)

--- next/web/src/global.d.ts
+++ next/web/src/global.d.ts
@@ -3,7 +3,7 @@
 interface ImportMetaEnv {
   VITE_LC_APP_ID: string;
   VITE_LC_APP_KEY: string;
-  VITE_LC_API_SERVER: string;
+  VITE_LEANCLOUD_API_HOST: string;
   VITE_SENTRY_WEB_DSN: string;
   VITE_ENABLE_LEANCLOUD_INTEGRATION: string;
 }
