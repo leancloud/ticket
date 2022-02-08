5c35b86aec3c572f751988da770d353d0be281fc
Ayumi Sarah <i@sdjdd.com>
2022-02-08 17:58:25 +0800

    feat(next/web): open link in new tab (#531)

--- next/web/src/App/Tickets/New/TicketForm/Fields/Help/index.tsx
+++ next/web/src/App/Tickets/New/TicketForm/Fields/Help/index.tsx
@@ -1,10 +1,18 @@
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown, { Components } from 'react-markdown';
 
 import style from './index.module.css';
+
+const components: Components = {
+  a: ({ node, ...props }) => <a {...props} target="_blank" />,
+};
 
 export function Help({ content }: { content?: string }) {
   if (!content) {
     return null;
   }
-  return <ReactMarkdown className={`markdown-body ${style.md}`}>{content}</ReactMarkdown>;
+  return (
+    <ReactMarkdown className={`markdown-body ${style.md}`} components={components}>
+      {content}
+    </ReactMarkdown>
+  );
 }
