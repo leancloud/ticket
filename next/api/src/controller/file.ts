cacca81bf09fe2aa4490b17cca19be2f40ef1fbd
Lee Yeh <lee55962698@gmail.com>
2022-04-28 23:47:30 +0800

    feat(api): POST /external-files

--- next/api/src/controller/file.ts
+++ next/api/src/controller/file.ts
@@ -1,8 +1,20 @@
-import { Controller, Get, Param, Query, ResponseBody, UseMiddlewares } from '@/common/http';
-import { FindModelPipe, ParseCsvPipe } from '@/common/pipe';
+import {
+  Body,
+  Controller,
+  CurrentUser,
+  Get,
+  Param,
+  Post,
+  Query,
+  ResponseBody,
+  UseMiddlewares,
+} from '@/common/http';
+import { FindModelPipe, ParseCsvPipe, ZodValidationPipe } from '@/common/pipe';
 import { auth, customerServiceOnly } from '@/middleware';
 import { File } from '@/model/File';
+import { User } from '@/model/User';
 import { FileResponse } from '@/response/file';
+import { z } from 'zod';
 
 @Controller('files')
 export class FileController {
@@ -22,5 +34,35 @@ export class FileController {
   @ResponseBody(FileResponse)
   async findOne(@Param('id', new FindModelPipe(File, { useMasterKey: true })) file: File) {
     return file;
+  }
+}
+
+const getFileNameFromURL = (url: string) => decodeURI(url).split('/').pop();
+
+const externalFileschema = z.array(
+  z.object({
+    url: z.string(),
+    name: z.string().optional(),
+    metaData: z.record(z.any()).optional(),
+  })
+);
+type ExternalFilesSchema = z.infer<typeof externalFileschema>;
+
+@UseMiddlewares(auth)
+@Controller('external-files')
+export class ExternalFileController {
+  @Post()
+  async batchSave(
+    @CurrentUser() currentUser: User,
+    @Body(new ZodValidationPipe(externalFileschema)) externalFiles: ExternalFilesSchema
+  ) {
+    const files = await File.createSome(
+      externalFiles.map(({ url, name, metaData }) => ({
+        url,
+        name: name ?? getFileNameFromURL(url),
+        metaData: { ...metaData, owner: currentUser.id },
+      }))
+    );
+    return files.map((file) => file.id);
   }
 }
--- next/api/src/model/File.ts
+++ next/api/src/model/File.ts
@@ -11,4 +11,7 @@ export class File extends Model {
 
   @field()
   url!: string;
+
+  @field()
+  metaData?: object
 }
