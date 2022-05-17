3f4e9737087e168026cb76740a1935dfad428dbb
sdjdd <i@sdjdd.com>
2022-05-17 11:00:14 +0800

    refactor: remove no effect await

--- next/api/src/model/ArticleTopic.ts
+++ next/api/src/model/ArticleTopic.ts
@@ -43,9 +43,7 @@ const getRawTopic = mem((id: string) => ArticleTopic.find(id), {
 export const getTopic = async (id: string) => {
   const topic = await getRawTopic(id);
   if (topic) {
-    topic.articles = await (await Promise.all(topic.articleIds.map(getPublicArticle))).filter(
-      isTruthy
-    );
+    topic.articles = (await Promise.all(topic.articleIds.map(getPublicArticle))).filter(isTruthy);
   }
   return topic;
 };
