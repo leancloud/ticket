7e69e2cefad40e7d74d237595b5e97e390d05677
Lee Yeh <lee55962698@gmail.com>
2022-06-18 15:38:16 +0800

    feat(in-app): navless mode

--- in-app/v1/src/components/Page/index.tsx
+++ in-app/v1/src/components/Page/index.tsx
@@ -5,23 +5,29 @@ import cx from 'classnames';
 import { ControlButton } from '@/components/ControlButton';
 import { ErrorBoundary } from '../ErrorBoundary';
 import styles from './index.module.css';
+import { useSearchParams } from 'react-router-dom';
+import classNames from 'classnames';
 
 export function PageHeader(props: ComponentPropsWithoutRef<'div'>) {
   const { t } = useTranslation();
+  const [search] = useSearchParams();
+  const showNav = search.get('nav') !== '0';
 
   return (
     <>
-      <div
-        id="page-header"
-        className={cx(
-          styles.header,
-          'shrink-0 z-20 top-0 px-[10px] sm:px-[108px] pt-[52px] sm:pt-[10px]',
-          props.className
-        )}
-      >
-        <ControlButton className="absolute top-3 sm:top-[10px] left-[10px] sm:left-[22px]" />
-      </div>
-      <div className="z-10 overflow-hidden">
+      {showNav && (
+        <div
+          id="page-header"
+          className={cx(
+            styles.header,
+            'shrink-0 z-20 top-0 px-[10px] sm:px-[108px] pt-[52px] sm:pt-[10px]',
+            props.className
+          )}
+        >
+          <ControlButton className="absolute top-3 sm:top-[10px] left-[10px] sm:left-[22px]" />
+        </div>
+      )}
+      <div className={classNames('z-10 overflow-hidden', { ['pt-[10px]']: !showNav })}>
         <h1
           className={`bg-white rounded-t-lg py-2 px-4  mx-[10px] sm:mx-[108px] text-center font-bold border-b border-gray-100`}
         >
