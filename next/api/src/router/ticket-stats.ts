a9c0c11bfc34558dd0be63a896c0b8305f57b1b1
lywTYH <763613923@qq.com>
2022-07-07 19:37:32 +0800

    fix: 统计包含父分类

--- next/api/src/router/ticket-stats.ts
+++ next/api/src/router/ticket-stats.ts
@@ -7,7 +7,7 @@ import { TicketStats } from '@/model/TicketStats';
 import { CategoryService } from '@/service/category';
 import { TicketStatusStats } from '@/model/TicketStatusStats';
 import { TicketStatusStatsResponse } from '@/response/ticket-stats';
-import { Reply } from '@/model/Reply';
+
 const router = new Router().use(auth, customerServiceOnly);
 
 const BaseSchema = {
@@ -15,60 +15,46 @@ const BaseSchema = {
   to: yup.date().required(),
   category: yup.string().optional(),
   customerService: yup.string().optional(),
-}
-const statsSchema = yup.object().shape(BaseSchema)
-const getCategoryIds = (categoryId?: string) => {
-  if (!categoryId) {
-    return;
-  }
-  return CategoryService.getSubCategories(categoryId).then(categories => {
-    return categories.length === 0 ? [categoryId] : categories.map(v => v.id)
-  })
-}
+};
 
-router.get('/',
-  async (ctx) => {
-    const { category, customerService, ...rest } = statsSchema.validateSync(ctx.query);
-    const categoryIds = await getCategoryIds(category)
-    const data = await TicketStats.fetchTicketStats({
-      ...rest,
-      customerServiceId: customerService,
-      categoryIds
-    })
-    ctx.body = data || {};
-  }
-);
+const statsSchema = yup.object().shape(BaseSchema);
+router.get('/', async (ctx) => {
+  const { category, customerService, ...rest } = statsSchema.validateSync(ctx.query);
+  const categoryIds = await getCategoryIds(category);
+  const data = await TicketStats.fetchTicketStats({
+    ...rest,
+    customerServiceId: customerService,
+    categoryIds,
+  });
+  ctx.body = data || {};
+});
 
-
-const fieldStatsSchema = yup.object().shape({
-  ...BaseSchema,
+const fieldStatsSchema = yup.object(BaseSchema).shape({
   fields: yup.string().required(),
 });
 router.get('/fields', async (ctx) => {
   const { category, customerService, fields, ...rest } = fieldStatsSchema.validateSync(ctx.query);
-  const categoryIds = category === '*' ? '*' : await getCategoryIds(category)
+  const categoryIds = category === '*' ? '*' : await getCategoryIds(category);
   const data = await TicketStats.fetchTicketFieldStats({
     ...rest,
     customerServiceId: customerService,
     categoryIds,
-    fields: fields.split(',')
-  })
+    fields: fields.split(','),
+  });
   ctx.body = data;
-})
+});
 
-const statusSchema = yup.object(_.pick(BaseSchema, 'from', 'to'));
+const statusSchema = yup.object(BaseSchema).pick(['from', 'to']);
 router.get('/status', async (ctx) => {
   const { from, to } = statusSchema.validateSync(ctx.query);
   const data = await TicketStatusStats.fetchTicketStatus({
     from,
-    to
-  })
-  ctx.body = data.map(value => new TicketStatusStatsResponse(value));
-})
+    to,
+  });
+  ctx.body = data.map((value) => new TicketStatusStatsResponse(value));
+});
 
-
-const detailSchema = yup.object().shape({
-  ...BaseSchema,
+const detailSchema = yup.object(BaseSchema).shape({
   field: yup.string().required(),
 });
 router.get('/details', async (ctx) => {
@@ -76,22 +62,27 @@ router.get('/details', async (ctx) => {
   const data = await TicketStats.fetchReplyDetails({
     ...rest,
     customerServiceId: customerService,
-    categoryId: category
-  })
+    categoryId: category,
+  });
   ctx.body = data || [];
-})
-
-const activeTicketCountSchema = yup.object().shape({
-  from: yup.date().required(),
-  to: yup.date().required(),
 });
+
+const activeTicketCountSchema = yup.object(BaseSchema).pick(['from', 'to']);
 router.get('/count', async (ctx) => {
   const params = activeTicketCountSchema.validateSync(ctx.query);
   const data = await TicketStats.fetchReplyDetails({
     ...params,
     field: 'naturalReplyTime',
     customerServiceId: '*',
-  })
+  });
   ctx.body = _(data).groupBy('nid').keys().valueOf().length;
-})
+});
 export default router;
+
+async function getCategoryIds(categoryId?: string) {
+  if (!categoryId) {
+    return;
+  }
+  const categories = await CategoryService.getSubCategories(categoryId);
+  return [categoryId, ...categories.map((v) => v.id)];
+}
