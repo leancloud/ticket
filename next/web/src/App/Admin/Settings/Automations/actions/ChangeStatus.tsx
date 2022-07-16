74b7218216b6796144d56ea47f0646f50de90caa
sdjdd <i@sdjdd.com>
2022-07-17 01:55:22 +0800

    refactor: react-hook-form recommended usage

--- next/web/src/App/Admin/Settings/Automations/actions/ChangeStatus.tsx
+++ next/web/src/App/Admin/Settings/Automations/actions/ChangeStatus.tsx
@@ -1,5 +1,4 @@
-import { Controller, useFormContext } from 'react-hook-form';
-import { get } from 'lodash-es';
+import { Controller } from 'react-hook-form';
 
 import { Form, Select } from '@/components/antd';
 import { actions, OperateAction } from '@/api/op-log';
@@ -11,22 +10,19 @@ const labels: Record<OperateAction, string> = {
   close: '关闭工单',
   reopen: '重新打开工单',
 };
+
 const options = actions.map((action) => ({
   value: action,
   label: labels[action],
 }));
 
 export function ChangeStatus({ path }: { path: string }) {
-  const { control, formState } = useFormContext();
-  const errors = get(formState.errors, path);
-
   return (
-    <Form.Item validateStatus={errors?.value ? 'error' : undefined}>
-      <Controller
-        control={control}
-        name={`${path}.value`}
-        rules={{ validate: (value) => value !== undefined }}
-        render={({ field }) => (
+    <Controller
+      name={`${path}.value`}
+      rules={{ required: true }}
+      render={({ field, fieldState: { error } }) => (
+        <Form.Item validateStatus={error ? 'error' : undefined}>
           <Select
             {...field}
             options={options}
@@ -34,8 +30,8 @@ export function ChangeStatus({ path }: { path: string }) {
             optionFilterProp="label"
             style={{ width: 200 }}
           />
-        )}
-      />
-    </Form.Item>
+        </Form.Item>
+      )}
+    />
   );
 }
