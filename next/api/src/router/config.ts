d0b755ec8784884384ecce18e79013562d5b73db
lywTYH <763613923@qq.com>
2022-07-25 14:42:31 +0800

    fix: add config key schema

--- next/api/src/router/config.ts
+++ next/api/src/router/config.ts
@@ -1,15 +1,24 @@
 import Router from '@koa/router';
 import _ from 'lodash';
+import * as yup from '@/utils/yup';
 import { auth, customerServiceOnly } from '@/middleware';
 import { Config } from '@/model/Config';
 
 const router = new Router().use(auth, customerServiceOnly);
-router.get('/:key', async (ctx) => {
-  ctx.body = await Config.get(ctx.params.key);
+
+const configKeySchema = yup.object({
+  key: yup.string().oneOf(['work_time', 'weekday']).required(),
 });
+
+router.get('/:key', async (ctx) => {
+  const { key } = configKeySchema.validateSync(ctx.params);
+  ctx.body = await Config.get(key);
+});
+
 router.patch('/:key', async (ctx) => {
-  const { value } = await Config.set(ctx.params.key, ctx.request.body);
-  ctx.body = value
+  const { key } = configKeySchema.validateSync(ctx.params);
+  const { value } = await Config.set(key, ctx.request.body);
+  ctx.body = value;
 });
 
 export default router;
