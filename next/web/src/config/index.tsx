a4c43a2008fa03035f2c725a06a10d7dd2f82226
Neko <neko@qwq.icu>
2022-09-26 15:55:11 +0800

    feat(UserLabel): add link to tap user info

--- next/web/src/config/index.tsx
+++ next/web/src/config/index.tsx
@@ -1,1 +1,9 @@
-export {};
+import { setConfig } from './config';
+
+setConfig('ticketDetail.userLabelOverlay', (user) => {
+  return (
+    <a href={`https://www.taptap.com/admin/user/edit/${user.username}`} target="__blank">
+      TapTap 用户信息
+    </a>
+  );
+});
