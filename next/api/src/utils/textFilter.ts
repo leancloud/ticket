75df5497a0c819ac58ba5b78cb1a6c5e97c8219c
Neko <neko@qwq.icu>
2022-09-26 15:23:03 +0800

    perf(next/api): automatically disable filter when lack of env vars

--- next/api/src/utils/textFilter.ts
+++ next/api/src/utils/textFilter.ts
@@ -11,10 +11,47 @@ export interface FilterOptions {
   escape?: boolean;
 }
 
+type ExtractArrayType<T extends ArrayLike<unknown>> = T extends ArrayLike<infer Res> ? Res : never;
+
 export class TextFilterService {
   private maxRetries = Number(process.env.TEXT_FILTER_MAX_RETRY) || 3;
   private isServerDown = false;
   private serverRecoveryTimeout = 60 * 1000;
+  private disable = false;
+  private host = '';
+  private clientId = '';
+  private serverSecret = '';
+  private scene = '';
+
+  constructor() {
+    const envVarList = [
+      'TDS_TEXT_FILTER_HOST',
+      'TDS_TEXT_FILTER_SCENE',
+      'TDS_CLIENT_ID',
+      'TDS_SERVER_SECRET',
+    ] as const;
+
+    const envVars = envVarList.reduce(
+      (acc, key) => ({ ...acc, [key]: process.env[key] }),
+      {} as Record<ExtractArrayType<typeof envVarList>, string | undefined>
+    );
+
+    const lackEnvVars = Object.entries(envVars).filter(([, value]) => !value);
+
+    if (lackEnvVars.length > 0) {
+      console.warn(
+        `Lack environment variable: ${lackEnvVars
+          .map(([name]) => name)
+          .join(', ')}, text filter disable.`
+      );
+      this.disable = true;
+    }
+
+    this.host = envVars['TDS_TEXT_FILTER_HOST'] as string;
+    this.clientId = envVars['TDS_CLIENT_ID'] as string;
+    this.scene = envVars['TDS_TEXT_FILTER_SCENE'] as string;
+    this.serverSecret = envVars['TDS_SERVER_SECRET'] as string;
+  }
 
   private setServerDown() {
     this.isServerDown = true;
@@ -24,14 +61,7 @@ export class TextFilterService {
   }
 
   async filter(input: string, { escape = true, requestOptions }: FilterOptions) {
-    if (!input || this.isServerDown) return input;
-
-    const {
-      TDS_TEXT_FILTER_HOST,
-      TDS_TEXT_FILTER_SCENE,
-      TDS_CLIENT_ID,
-      TDS_SERVER_SECRET,
-    } = process.env;
+    if (!input || this.isServerDown || this.disable) return input;
 
     const sha1 = createHash('sha1').update(input).digest('hex');
 
@@ -42,7 +72,7 @@ export class TextFilterService {
         }>(
           '/v2/text/check',
           {
-            scene: TDS_TEXT_FILTER_SCENE as string,
+            scene: this.scene,
             data: {
               ...requestOptions,
               user_id: `ticket-${requestOptions.user_id}`,
@@ -51,10 +81,10 @@ export class TextFilterService {
             },
           },
           {
-            baseURL: TDS_TEXT_FILTER_HOST as string,
+            baseURL: this.host,
             headers: {
-              'X-Client-ID': TDS_CLIENT_ID as string,
-              'X-Server-Secret': TDS_SERVER_SECRET as string,
+              'X-Client-ID': this.clientId,
+              'X-Server-Secret': this.serverSecret,
               'Content-Type': 'application/json',
             },
             timeout: 1000,
