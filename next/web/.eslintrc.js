67569dd1b9048978b485bf0a560a3e830519a5a6
猫猫 <neko@qwq.icu>
2022-10-24 16:07:16 +0800

    chore: update eslint config (#680)

--- next/web/.eslintrc.js
+++ next/web/.eslintrc.js
@@ -10,4 +10,13 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
   },
+
+  rules: {
+    'react/display-name': 'off',
+    'react/no-children-prop': 'off',
+    'react/no-unknown-property': 'warn',
+    'react/jsx-key': 'warn',
+    'react/jsx-no-target-blank': ['warn', { enforceDynamicLinks: 'never' }],
+    'react/prop-types': 'off',
+  },
 };
