87cd07dc46251480fd7ff6cff7a2aeefc5fbda42
sdjdd <i@sdjdd.com>
2022-11-03 15:08:37 +0800

    feat(next/web): InternalBadge tooltip

--- next/web/src/App/Admin/Tickets/Ticket/components/InternalBadge.tsx
+++ next/web/src/App/Admin/Tickets/Ticket/components/InternalBadge.tsx
@@ -1,7 +1,11 @@
+import { Tooltip } from '@/components/antd';
+
 export function InternalBadge() {
   return (
-    <span className="bg-gray-500 text-white rounded-sm text-sm px-1 ml-1 font-semibold">
-      internal
-    </span>
+    <Tooltip title="仅员工可见">
+      <span className="bg-gray-500 text-white rounded-sm text-sm px-1 ml-1 font-semibold">
+        internal
+      </span>
+    </Tooltip>
   );
 }
