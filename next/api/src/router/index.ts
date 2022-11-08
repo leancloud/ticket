fa4bc19adfc2ae0e41d768a94814a819a5672e4e
sdjdd <i@sdjdd.com>
2022-11-08 17:33:58 +0800

    fix(next/api): remove outdated route mapping

--- next/api/src/router/index.ts
+++ next/api/src/router/index.ts
@@ -19,7 +19,6 @@ const router = new Router({ prefix: '/api/2' }).use(catchYupError, catchLCError,
 router.use('/tickets', ticket.routes());
 router.use('/organizations', organization.routes());
 router.use('/unread', unread.routes());
-router.use('/products', unread.routes());
 router.use('/notifications', notification.routes());
 router.use('/articles', article.routes());
 router.use('/triggers', trigger.routes());
