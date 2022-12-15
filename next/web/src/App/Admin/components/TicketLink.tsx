c84f44e364208956b9f4c1128680fc1906d375ad
Li Ye <liye@xd.com>
2022-12-15 15:33:44 +0800

    feat: optimize TicketTable

--- next/web/src/App/Admin/components/TicketLink.tsx
+++ next/web/src/App/Admin/components/TicketLink.tsx
@@ -1,15 +1,24 @@
 import { TicketSchema } from '@/api/ticket';
+import classNames from 'classnames';
 
-export function TicketLink({ ticket }: { ticket: TicketSchema }) {
+export function TicketLink({
+  ticket,
+  className,
+  showId = true,
+}: {
+  ticket: TicketSchema;
+  className?: string;
+  showId?: boolean;
+}) {
   return (
     <a
-      className="mt-1.5 font-bold max-w-full"
+      className={classNames(className, 'mt-1.5 font-bold')}
       title={ticket.title}
       href={`/tickets/${ticket.nid}`}
       target="_blank"
     >
       <span>{ticket.title}</span>
-      <span className="ml-1 text-[#6f7c87]">#{ticket.nid}</span>
+      {showId && <span className="ml-1 text-[#6f7c87]">#{ticket.nid}</span>}
     </a>
   );
 }
