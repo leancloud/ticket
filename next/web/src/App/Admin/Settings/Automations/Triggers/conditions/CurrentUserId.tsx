41f9fe9a8a7967fb0a42d62ba43fcfc91c71bfa5
sdjdd <i@sdjdd.com>
2023-01-06 15:19:53 +0800

    fix: CurrentUserId.tsx

--- next/web/src/App/Admin/Settings/Automations/Triggers/conditions/CurrentUserId.tsx
+++ next/web/src/App/Admin/Settings/Automations/Triggers/conditions/CurrentUserId.tsx
@@ -1,16 +1,12 @@
 import { useMemo } from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
-import { get } from 'lodash-es';
+import { Controller } from 'react-hook-form';
 
-import { Form, Select } from '@/components/antd';
 import { useCustomerServices } from '@/api/user';
+import { Form, Select } from '@/components/antd';
 
 const { Option } = Select;
 
 export function CurrentUserId({ path }: { path: string }) {
-  const { control, formState } = useFormContext();
-  const errors = get(formState.errors, path);
-
   const { data: assignees } = useCustomerServices();
   const options = useMemo(() => {
     return [
@@ -21,27 +17,25 @@ export function CurrentUserId({ path }: { path: string }) {
 
   return (
     <>
-      <Form.Item>
-        <Controller
-          control={control}
-          name={`${path}.op`}
-          rules={{ required: true }}
-          defaultValue="is"
-          render={({ field }) => (
+      <Controller
+        name={`${path}.op`}
+        rules={{ required: true }}
+        defaultValue="is"
+        render={({ field }) => (
+          <Form.Item>
             <Select {...field} style={{ width: 160 }}>
               <Option value="is">是</Option>
               <Option value="isNot">不是</Option>
             </Select>
-          )}
-        />
-      </Form.Item>
+          </Form.Item>
+        )}
+      />
 
-      <Form.Item validateStatus={errors?.value ? 'error' : undefined}>
-        <Controller
-          control={control}
-          name={`${path}.value`}
-          rules={{ validate: (value) => value !== undefined }}
-          render={({ field }) => (
+      <Controller
+        name={`${path}.value`}
+        rules={{ validate: (value) => value !== undefined }}
+        render={({ field, fieldState: { error } }) => (
+          <Form.Item validateStatus={error ? 'error' : undefined}>
             <Select
               {...field}
               showSearch
@@ -50,9 +44,9 @@ export function CurrentUserId({ path }: { path: string }) {
               optionFilterProp="label"
               style={{ width: 200 }}
             />
-          )}
-        />
-      </Form.Item>
+          </Form.Item>
+        )}
+      />
     </>
   );
 }
