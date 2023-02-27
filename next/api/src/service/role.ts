38d0b3afdfc4eb6625ac08ea9f4c3578c11f2585
sdjdd <i@sdjdd.com>
2023-02-27 12:23:22 +0800

    feat: get system roles for user

--- next/api/src/service/role.ts
+++ next/api/src/service/role.ts
@@ -1,8 +1,16 @@
 import AV from 'leancloud-storage';
+import LRUCache from 'lru-cache';
 import { Role } from '@/model/Role';
 import { User } from '@/model/User';
 
+const SYSTEM_ROLE_NAMES = ['admin', 'customerService', 'staff'];
+
 export class RoleService {
+  private systemRolesCache = new LRUCache<string, string[]>({
+    max: 10000,
+    ttl: 1000 * 60,
+  });
+
   getRoleByName(name: string) {
     return Role.queryBuilder().where('name', '==', name).first({ useMasterKey: true });
   }
@@ -26,6 +34,23 @@ export class RoleService {
       .relatedTo(Role, 'users', roleId)
       .limit(1000)
       .find({ useMasterKey: true });
+  }
+
+  async getSystemRolesForUser(userId: string) {
+    const cached = this.systemRolesCache.get(userId);
+    if (cached) {
+      return cached;
+    }
+
+    const roles = await Role.queryBuilder()
+      .select('name')
+      .where('users', '==', User.ptr(userId))
+      .where('name', 'in', SYSTEM_ROLE_NAMES)
+      .find();
+    const names = roles.map((role) => role.name);
+
+    this.systemRolesCache.set(userId, names);
+    return names;
   }
 }
 
