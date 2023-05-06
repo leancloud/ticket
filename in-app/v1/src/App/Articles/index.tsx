leancloud/ticket

commit f006fa3075714065afa5aed29161521bb0857b97

fix(in-app): remove hard-coded styles
which lead to text-overflow in certain languages

in-app/v1/src/components/Button/index.tsx
@@ -14,7 +14,7 @@ export function Button({ as = 'button', secondary, className, ...props }: Button
     ...props,
     className: classNames(
       styles.button,
-      'leading-9 px-4 rounded-full text-center font-bold select-none',
+      'leading-9 px-2 min-w-[4rem] rounded-full text-center font-bold select-none',
       secondary? styles.secondary : styles.primary,
       secondary ? 'border border-gray-200 hover:text-tapBlue hover:border-tapBlue focus:text-tapBlue focus:border-tapBlue' : 'bg-tapBlue text-white',
       className

in-app/v1/src/App/Tickets/Ticket/Evaluation/index.tsx
@@ -60,7 +60,7 @@ export function NewEvaluation({ initData, loading, onSubmit }: NewEvaluationProp
   };
 
   return (
-    <div className="p-5 border-t border-dashed border-gray-300 text-sm">
+    <div className="py-5 px-4 border-t border-dashed border-gray-300 text-sm">
       <div className="text-gray-600">{t('evaluation.title')}</div>
 
       <div className="py-6">
@@ -90,7 +90,7 @@ export function NewEvaluation({ initData, loading, onSubmit }: NewEvaluationProp
           onChange={(e) => setContent(e.target.value)}
         />
         <Button
-          className="ml-2 w-16 text-[13px] leading-[30px]"
+          className="ml-2 text-[13px] leading-[30px]"
           disabled={star === undefined || loading}
           onClick={handleCommit}
         >

in-app/v1/src/App/Tickets/Ticket/ReplyInput/index.tsx
@@ -64,7 +64,7 @@ export function ReplyInput({ onCommit }: ReplyInputProps) {
           onClick={() => setShow(true)}
         />
         <Button
-          className="shrink-0 ml-2 w-16 leading-[30px] text-[13px]"
+          className="shrink-0 ml-2 leading-[30px] text-[13px]"
           disabled={!commitable}
           onClick={handleCommit}
         >
@@ -114,7 +114,7 @@ function AdvancedReplyInput({
   return (
     <div className="bg-[#FAFAFA] border-t border-gray-100 pb-[env(safe-area-inset-bottom)]">
       <div className="relative flex px-3 py-2">
-        <div className="bg-white grow rounded-2xl border pl-3 pr-[34px] max-h-[200px] sm:max-h-[140px] overflow-y-auto text-sm">
+        <div className="bg-white grow rounded-2xl border pl-3 pr-[34px] max-h-[200px] sm:max-h-[140px] overflow-y-auto text-sm relative">
           <Editor
             autoFocus
             placeholder={t('reply.input_content_hint')}
@@ -122,7 +122,7 @@ function AdvancedReplyInput({
             onChange={onChange}
           />
           <MiniUploader
-            className="absolute right-[85px] bottom-[9px] w-[34px] h-[30px]"
+            className="absolute right-1 top-0 w-[34px] h-[30px]"
             onUpload={(files) => onUpload(files[0])}
           />
           {files && files.length > 0 && (
@@ -130,7 +130,7 @@ function AdvancedReplyInput({
           )}
         </div>
         <Button
-          className="shrink-0 w-16 mt-auto ml-2 leading-[30px] text-[13px]"
+          className="shrink-0 mt-auto ml-2 leading-[30px] text-[13px]"
           disabled={disabled}
           onClick={onCommit}
         >

in-app/v1/src/App/Tickets/Ticket/index.tsx
@@ -92,7 +92,7 @@ function ExpandButton({ expand, onClick }: ExpandButtonProps) {
 function TicketAttribute({ title, children }: PropsWithChildren<{ title: string }>) {
   return (
     <>
-      <label className="min-w-[2em] max-w-[5em] text-[#BFBFBF]">{title}</label>
+      <label className="min-w-[2em] max-w-[8em] text-[#BFBFBF]">{title}</label>
       <div className="text-[#666] overflow-hidden">{children}</div>
     </>
   );

commit ab5c5c9d5751d11124c98f594c99f5c605985908

fix(in-app): make buttons more adaptive
fix vote buttons broken by f006fa3075714065afa5aed29161521bb0857b97

in-app/v1/src/App/Articles/index.tsx
@@ -70,7 +70,7 @@ function Feedback({ articleId }: { articleId: string }) {
           <Button
             onClick={() => vote(1)}
             secondary
-            className="flex flex-row items-center min-w-[32px] h-[22px] bg-white text-[#888]"
+            className="flex flex-row items-center justify-center h-[22px] bg-white text-[#888]"
             disabled={isLoading}
           >
             <ThumbUpIcon className="w-[14px] h-[14px] inline-block align-middle" />
@@ -78,7 +78,7 @@ function Feedback({ articleId }: { articleId: string }) {
           <Button
             onClick={() => vote(-1)}
             secondary
-            className="flex items-center min-w-[32px] h-[22px] bg-white ml-4 text-[#888] hover:!text-red hover:!border-red focus:!text-red focus:!border-red"
+            className="flex items-center justify-center h-[22px] bg-white ml-3 text-[#888] hover:!text-red hover:!border-red focus:!text-red focus:!border-red"
             disabled={isLoading}
           >
             <ThumbDownIcon className="w-[14px] h-[14px] inline-block align-middle" />
@@ -137,7 +137,7 @@ function ArticleDetail() {
           <div className="px-4 py-5 text-[12px] leading-[1.5] text-[#666] text-center">
             <p>{t('notice.hint')}</p>
             <Link to="/categories">
-              <Button secondary className="px-8 text-base mt-2 text-tapBlue font-bold">
+              <Button secondary className="!px-8 text-base mt-2 text-tapBlue font-bold">
                 {t('feedback.submit')}
               </Button>
             </Link>

in-app/v1/src/components/Button/index.tsx
@@ -14,9 +14,11 @@ export function Button({ as = 'button', secondary, className, ...props }: Button
     ...props,
     className: classNames(
       styles.button,
-      'leading-9 px-2 min-w-[4rem] rounded-full text-center font-bold select-none',
-      secondary? styles.secondary : styles.primary,
-      secondary ? 'border border-gray-200 hover:text-tapBlue hover:border-tapBlue focus:text-tapBlue focus:border-tapBlue' : 'bg-tapBlue text-white',
+      'leading-9 px-[0.75em] min-w-[4.5em] rounded-full text-center font-bold select-none',
+      secondary ? styles.secondary : styles.primary,
+      secondary
+        ? 'border border-gray-200 hover:text-tapBlue hover:border-tapBlue focus:text-tapBlue focus:border-tapBlue'
+        : 'bg-tapBlue text-white',
       className
     ),
   });