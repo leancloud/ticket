8458626408855c7ca9df4d0082375f9feca0ef04
Neko <neko@qwq.icu>
2023-07-10 11:28:57 +0800

    refactor(in-app): persist input after component unmount

--- in-app/v1/src/App/Home/AiClassify.tsx
+++ in-app/v1/src/App/Home/AiClassify.tsx
@@ -2,112 +2,127 @@ import { ClassifyResult, useClassifyTicket } from '@/api/category';
 import { PageContent } from '@/components/Page';
 import { EditableIcon } from '@/icons/EditableIcon';
 import { useRootCategory } from '@/states/root-category';
-import { useState, useCallback, ChangeEventHandler, ComponentPropsWithoutRef, memo } from 'react';
+import {
+  useState,
+  useCallback,
+  ChangeEventHandler,
+  ComponentPropsWithoutRef,
+  memo,
+  KeyboardEventHandler,
+} from 'react';
 import { useTranslation } from 'react-i18next';
 import cx from 'classnames';
 import LoadingIcon from '@/icons/LoadingIcon.png';
 import { DoneIcon } from '@/icons/Done';
 import { Link } from 'react-router-dom';
 import { FailedIcon } from '@/icons/FailedIcon';
-import { useSetContent } from '@/states/content';
+import {
+  useSetContent,
+  useClassifyCategoryLoadable,
+  useContent,
+  useClassifyCategory,
+} from '@/states/content';
 import { Button } from '@/components/Button';
 
-const MaxLength = 100;
+const MaxLength = 150;
 
-interface InputProps {
-  onSubmit?: (value: string) => void;
-}
+const Input = memo(({ className, ...props }: ComponentPropsWithoutRef<'div'>) => {
+  const { t } = useTranslation();
 
-const Input = memo(
-  ({
-    onSubmit,
-    className,
-    ...props
-  }: InputProps & Omit<ComponentPropsWithoutRef<'div'>, keyof InputProps>) => {
-    const { t } = useTranslation();
+  const content = useContent();
+  const setContent = useSetContent();
 
-    const [value, setValue] = useState('');
-    const [editable, setEditable] = useState(true);
+  const [value, setValue] = useState(content ?? '');
+  const [editable, setEditable] = useState(!content);
 
-    const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>((e) => {
-      setValue(e.target.value);
-    }, []);
+  const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>((e) => {
+    setValue(e.target.value);
+  }, []);
 
-    const handleSubmit = useCallback(() => {
-      setEditable(false);
-      onSubmit?.(value);
-    }, [value]);
+  const handleSubmit = useCallback(() => {
+    setEditable(false);
+    setContent(value);
+  }, [value, setContent]);
 
-    const handleEdit = useCallback(() => {
-      setEditable(true);
-    }, []);
+  const handleEdit = useCallback(() => {
+    setEditable(true);
+  }, []);
 
-    return (
-      <>
-        <div className="flex flex-row justify-between">
-          <h2 className="font-bold">{t('category.classify.title')}</h2>
-          {editable && (
-            <span className={cx(value.length > MaxLength ? 'text-red' : 'text-[#B9BEC1]')}>
-              {value.length}/{MaxLength}
-            </span>
-          )}
-        </div>
-        <div
-          className={cx(
-            'mt-3',
-            'rounded-2xl',
-            'bg-black/[0.02]',
-            'py-3',
-            'px-4',
-            'flex',
-            'flex-row',
-            'justify-between',
-            'items-center',
-            !editable && 'w-fit',
-            className
-          )}
-          {...props}
-        >
-          {editable ? (
-            <>
-              <input
-                className="placeholder:text-[#BFBFBF] bg-transparent flex-grow"
-                onChange={handleChange}
-                value={value}
-                placeholder={t('category.classify.placeholder')}
-              />
-              <Button
-                onClick={handleSubmit}
-                disabled={!value.length || value.length > MaxLength}
-                className={cx(
-                  value.length > MaxLength
-                    ? 'text-red'
-                    : value.length
-                    ? 'text-tapBlue'
-                    : 'text-[#888888]',
-                  'transition-colors',
-                  'rounded-[15px]',
-                  'h-[1.875rem]',
-                  'flex',
-                  'items-center'
-                )}
-              >
-                {t('general.next_step')}
-              </Button>
-            </>
-          ) : (
-            <>
-              <span>{value}</span>
-              <button className="text-tapBlue ml-2 h-[1.875rem]" onClick={handleEdit}>
-                <EditableIcon />
-              </button>
-            </>
-          )}
-        </div>
-      </>
-    );
-  }
-);
+  const handlePressEnter = useCallback<KeyboardEventHandler<HTMLInputElement>>(
+    (e) => {
+      if (e.key === 'Enter') {
+        handleSubmit();
+      }
+    },
+    [handleSubmit]
+  );
+
+  return (
+    <>
+      <div className="flex flex-row justify-between">
+        <h2 className="font-bold">{t('category.classify.title')}</h2>
+        {editable && (
+          <span className={cx(value.length > MaxLength ? 'text-red' : 'text-[#B9BEC1]')}>
+            {value.length}/{MaxLength}
+          </span>
+        )}
+      </div>
+      <div
+        className={cx(
+          'mt-3',
+          'rounded-2xl',
+          'bg-black/[0.02]',
+          'py-3',
+          'px-4',
+          'flex',
+          'flex-row',
+          'justify-between',
+          'items-center',
+          !editable && 'w-fit',
+          className
+        )}
+        {...props}
+      >
+        {editable ? (
+          <>
+            <input
+              className="placeholder:text-[#BFBFBF] bg-transparent flex-grow"
+              onChange={handleChange}
+              onKeyPress={handlePressEnter}
+              value={value}
+              placeholder={t('category.classify.placeholder')}
+            />
+            <Button
+              onClick={handleSubmit}
+              disabled={!value.length || value.length > MaxLength}
+              className={cx(
+                value.length > MaxLength
+                  ? 'text-red'
+                  : value.length
+                  ? 'text-tapBlue'
+                  : 'text-[#888888]',
+                'transition-colors',
+                'rounded-[15px]',
+                'h-[1.875rem]',
+                'flex',
+                'items-center'
+              )}
+            >
+              {t('general.next_step')}
+            </Button>
+          </>
+        ) : (
+          <>
+            <span>{value}</span>
+            <button className="text-tapBlue ml-2 h-[1.875rem]" onClick={handleEdit}>
+              <EditableIcon />
+            </button>
+          </>
+        )}
+      </div>
+    </>
+  );
+});
 
 const Loading = memo(() => {
   const { t } = useTranslation();
@@ -122,8 +137,10 @@ const Loading = memo(() => {
   );
 });
 
-const Result = memo(({ data }: { data?: ClassifyResult }) => {
+const Result = memo(({ categoryId }: { categoryId: string }) => {
   const { t } = useTranslation();
+
+  const data = useClassifyCategory(categoryId);
 
   return (
     <div>
@@ -164,31 +181,20 @@ const Result = memo(({ data }: { data?: ClassifyResult }) => {
 
 export const AiClassify = ({ className }: { className?: string }) => {
   const category = useRootCategory();
-  const setContent = useSetContent();
 
-  const { data, mutate: classify, isLoading, isError } = useClassifyTicket({
-    onError: console.error,
-  });
-
-  const handleSubmit = useCallback(
-    (content: string) => {
-      classify({ categoryId: category.id, content });
-      setContent(content);
-    },
-    [classify, category, setContent]
-  );
+  const data = useClassifyCategoryLoadable(category.id);
 
   return (
     <PageContent className={className} shadow>
-      <Input onSubmit={handleSubmit} />
-      {(data || isLoading || isError) && (
+      <Input />
+      {(data.state === 'loading' || data.state === 'hasError' || data.getValue()) && (
         <div
           className={cx(
             'px-4 py-3 bg-gradient-to-b from-[#F2FDFE] to-[#F2FDFE]/[0.2] rounded-2xl mt-[0.62rem]',
             className
           )}
         >
-          {isLoading ? <Loading /> : <Result data={data} />}
+          {data.state === 'loading' ? <Loading /> : <Result categoryId={category.id} />}
         </div>
       )}
     </PageContent>
