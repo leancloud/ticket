d0829f3a8e0a519478588318cfc476146d703aee
sdjdd <i@sdjdd.com>
2023-11-30 11:44:23 +0800

    feat(next/api): [queue] reuse connection

--- next/api/src/queue/index.ts
+++ next/api/src/queue/index.ts
@@ -1,10 +1,35 @@
 import Bull from 'bull';
+import Redis, { Redis as IRedis } from 'ioredis';
 
 const QUEUE_REDIS = process.env.REDIS_URL_QUEUE ?? 'redis://127.0.0.1:6379';
+
+let client: IRedis | undefined;
+let subscriber: IRedis | undefined;
 
 export function createQueue<T>(name: string, options?: Bull.QueueOptions): Bull.Queue<T> {
   if (process.env.NODE_ENV === 'staging') {
     name += '_stg';
   }
-  return new Bull<T>(name, QUEUE_REDIS, options);
+  return new Bull<T>(name, {
+    ...options,
+    // https://github.com/OptimalBits/bull/blob/develop/PATTERNS.md#reusing-redis-connections
+    createClient: (type, redisOptions) => {
+      switch (type) {
+        case 'client':
+          if (!client) {
+            client = new Redis(QUEUE_REDIS, redisOptions);
+          }
+          return client;
+        case 'subscriber':
+          if (!subscriber) {
+            subscriber = new Redis(QUEUE_REDIS, redisOptions);
+          }
+          return subscriber;
+        case 'bclient':
+          return new Redis(QUEUE_REDIS, redisOptions);
+        default:
+          throw new Error(`Unexpected connection type: ${type}`);
+      }
+    },
+  });
 }
