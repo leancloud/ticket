915009a08135cf1837a93c3db687d6d7c67f515c
Uffy <uffywen@gmail.com>
2024-05-31 16:30:00 +0800

    feat: allows to print export job queue and cancel job

--- api/next-shim.js
+++ api/next-shim.js
@@ -41,14 +41,18 @@ AV.Cloud.define('dailyPushStatsToSlack', () => dailyPushStatsToSlack())
 AV.Cloud.define('weeklyPushStatsToSlack', () => weeklyPushStatsToSlack())
 AV.Cloud.define('monthlyPushStatsToSlack', () => monthlyPushStatsToSlack())
 
-const { cancelTicketExportJob } = require('../next/api/dist/ticket/export')
-AV.Cloud.define('removeExportJob', (req) => {
+const { cancelTicketExportJob, getTicketExportJobInfo } = require('../next/api/dist/ticket/export')
+AV.Cloud.define('cancelExportJob', (req) => {
   if (!req.params.jobId) {
     console.error('Cloud Function - removeExportJob: missing jobId')
     return
   }
 
   cancelTicketExportJob(req.params.jobId)
+})
+
+AV.Cloud.define('getTicketExportJobInfo', () => {
+  getTicketExportJobInfo()
 })
 
 AV.Cloud.onLogin((request) => {
